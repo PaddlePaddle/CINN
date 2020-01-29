e68ca2e8cd970a8994517089ea1915547dbd2ff6
Chunwei <yanchunwei@outlook.com>
2020-01-29 16:18:52 +0800

    add more operations

--- cinn/ir/operation.h
+++ cinn/ir/operation.h
@@ -2,6 +2,7 @@
 
 #include <map>
 #include <string>
+#include <utility>
 #include <vector>
 
 #include "cinn/ir/buffer.h"
@@ -30,6 +31,51 @@ struct ExternOp : public _Operation_ {
                         std::vector<Buffer> input_placeholders,
                         std::vector<Buffer> output_placeholders,
                         Stmt body);
+};
+
+/**
+ * @brief A placeholder op represents an input placeholder.
+ */
+struct PlaceholderOp : public _Operation_ {
+  //! The shape of the input.
+  std::vector<Expr> shape;
+  //! The data type of the input.
+  Type dtype;
+
+  static Operation Make(std::string name, std::vector<Expr> shape, Type dtype) {
+    auto n   = common::make_shared<PlaceholderOp>();
+    n->name  = name;
+    n->shape = shape;
+    n->dtype = dtype;
+    return Operation(n);
+  }
+};
+
+/**
+ * @brief A Compute op that compute a tensor on certain domain.
+ */
+struct ComputeOp : public _Operation_ {
+  //! Vars on each axis.
+  std::vector<Var> axis;
+  //! Var on each reduction axis, if the body is a Reduction.
+  std::vector<Var> reduce_axis;
+  //! The compute expression.
+  std::vector<Expr> body;
+
+  ComputeOp() = default;
+
+  static Operation Make(std::string name,
+                        std::string tag,
+                        std::map<std::string, IrNodeRef> attrs,
+                        std::vector<Var> axis,
+                        std::vector<Expr> body) {
+    auto n   = common::make_shared<ComputeOp>();
+    n->name  = std::move(name);
+    n->tag   = std::move(tag);
+    n->attrs = std::move(attrs);
+    n->body  = std::move(body);
+    return Operation(n);
+  }
 };
 
 }  // namespace ir
