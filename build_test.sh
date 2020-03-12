fdb97dff626751f60e6e558c5031e112df7c4a41
Superjomn <yanchunwei@outlook.com>
2020-03-12 18:17:54 +0800

    fix build

--- build_test.sh
+++ build_test.sh
@@ -11,11 +11,12 @@ touch tests/test02_matmul.h
 touch tests/test01_elementwise_add.h
 touch tests/test02_matmul_tile.h
 
-# run the test_mains and generate c code
-make -j6
-set +e
-ctest -V
-set -e
+make test01_elementwise_add_main -j6
+make test02_matmul_main -j6
 
+ctest -R test01_elementwise_add_main
+ctest -R test02_matmul_main
+
+# run the test_mains and generate c code
 make -j6
 ctest -V
