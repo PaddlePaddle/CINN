f86b309aecc97174dc6b58f8bbf045c9edea13fa
Superjomn <yanchunwei@outlook.com>
2020-03-22 14:34:08 +0800

    add loop permutation tests to matmul

--- tests/test02_matmul_main.cc
+++ tests/test02_matmul_main.cc
@@ -137,8 +137,6 @@ TEST(matmul, Blocking) {
     C->stage()->Reorder({i_outer, j_outer, k_outer, k_inner, i_inner, j_inner});
   }
 
-  // C_init->stage()->ComputeAt(C->stage(), 3);
-
   Module module("module_block", target);
   auto funcs = Lower("matmul_block", {A, B, C, C_init});
   ASSERT_EQ(funcs.size(), 1UL);
@@ -167,7 +165,7 @@ TEST(matmul, Vectorization) {
   auto C = Compute(
       {M, N}, [&](Var i, Var j) { return Sum(A(i, k) * B(k, j), k); }, "C", k);
   C->Bind(C_buf);
-  ASSERT_EQ(C->buffer_depended_tensor_names().size(), 1UL);
+  // ASSERT_EQ(C->buffer_depended_tensor_names().size(), 1UL);
 
   Target target;
   target.arch = Target::Arch ::X86;
@@ -186,8 +184,6 @@ TEST(matmul, Vectorization) {
     C->stage()->Vectorize(j_inner, 8);
   }
 
-  // C_init->stage()->ComputeAt(C->stage(), 3);
-
   Module module("module_vectorize", target);
   auto funcs = Lower("matmul_vectorize", {A, B, C, C_init});
   ASSERT_EQ(funcs.size(), 1UL);
@@ -198,6 +194,106 @@ TEST(matmul, Vectorization) {
   CodeGenCX86 compiler(target, CodeGenCX86::Feature::AVX256);
   Outputs outputs;
   outputs = outputs.c_header("./test02_matmul_vectorize.h").c_source("./test02_matmul_vectorize.cc");
+  compiler.Compile(module, outputs);
+}
+
+TEST(matmul, LoopPermutation) {
+  Placeholder<float> A("A", {M, K});
+  Placeholder<float> B("B", {K, N});
+
+  Var k(K, "k");
+  Buffer C_buf(Float(32));
+
+  int bn = 32;
+
+  auto C_init = Compute(
+      {M, N}, [&](Var i, Var j) { return Expr(0.f); }, "C_init");
+  C_init->Bind(C_buf);
+  auto C = Compute(
+      {M, N}, [&](Var i, Var j) { return Sum(A(i, k) * B(k, j), k); }, "C", k);
+  C->Bind(C_buf);
+  ASSERT_EQ(C->buffer_depended_tensor_names().size(), 1UL);
+
+  Target target;
+  target.arch = Target::Arch ::X86;
+  target.bits = Target::Bit ::k32;
+  target.os   = Target::OS ::Linux;
+
+  // Blocking by loop tiling.
+  {
+    Iterator i_outer, i_inner, j_outer, j_inner;
+    Iterator k_outer, k_inner;
+
+    std::tie(i_outer, i_inner, j_outer, j_inner) = C->stage()->Tile(0, 1, bn, bn);
+    std::tie(k_outer, k_inner)                   = C->stage()->Split("k", 4);
+
+    C->stage()->Reorder({i_outer, j_outer, k_outer, i_inner, k_inner, j_inner});
+
+    C->stage()->Vectorize(j_inner, 8);
+  }
+
+  Module module("module_loop_permutation", target);
+  auto funcs = Lower("matmul_loop_permutation", {A, B, C, C_init});
+  ASSERT_EQ(funcs.size(), 1UL);
+
+  auto func = Optimize(funcs.front());
+  module.Append(ir::LoweredFunc(func.As<ir::_LoweredFunc_>()));
+
+  CodeGenCX86 compiler(target, CodeGenCX86::Feature::AVX256);
+  Outputs outputs;
+  outputs = outputs.c_header("./test02_matmul_loop_permutation.h").c_source("./test02_matmul_loop_permutation.cc");
+  compiler.Compile(module, outputs);
+}
+
+TEST(matmul, ArrayPacking) {
+  Placeholder<float> A("A", {M, K});
+  Placeholder<float> B("B", {K, N});
+
+  Var k(K, "k");
+  Buffer C_buf(Float(32));
+
+  int bn = 32;
+
+  auto C_init = Compute(
+      {M, N}, [&](Var i, Var j) { return Expr(0.f); }, "C_init");
+  C_init->Bind(C_buf);
+  auto packedB = Compute(
+      {N / bn, K, bn}, [&](Expr x, Expr y, Expr z) { return B(y, x * bn + z); }, "packedB");
+  Buffer packedB_buf(packedB->type());
+  packedB->Bind(packedB_buf);
+
+  auto C = Compute(
+      {M, N}, [&](Expr i, Expr j) { return Sum(A(i, k) * packedB(j / bn, k, j % bn), k); }, "C", k);
+  C->Bind(C_buf);
+
+  ASSERT_EQ(C->buffer_depended_tensor_names().size(), 1UL);
+
+  Target target;
+  target.arch = Target::Arch ::X86;
+  target.bits = Target::Bit ::k32;
+  target.os   = Target::OS ::Linux;
+
+  {
+    Iterator i_outer, i_inner, j_outer, j_inner;
+    Iterator k_outer, k_inner;
+
+    std::tie(i_outer, i_inner, j_outer, j_inner) = C->stage()->Tile(0, 1, bn, bn);
+    std::tie(k_outer, k_inner)                   = C->stage()->Split("k", 4);
+
+    C->stage()->Reorder({i_outer, j_outer, k_outer, i_inner, k_inner, j_inner});
+    // C->stage()->Vectorize(j_inner, 8);
+  }
+
+  Module module("module_array_packing", target);
+  auto funcs = Lower("matmul_array_packing", {A, B, C, C_init, packedB});
+  ASSERT_EQ(funcs.size(), 1UL);
+
+  auto func = Optimize(funcs.front());
+  module.Append(ir::LoweredFunc(func.As<ir::_LoweredFunc_>()));
+
+  CodeGenCX86 compiler(target, CodeGenCX86::Feature::AVX256);
+  Outputs outputs;
+  outputs = outputs.c_header("./test02_matmul_array_packing.h").c_source("./test02_matmul_array_packing.cc");
   compiler.Compile(module, outputs);
 }
 
