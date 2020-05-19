b368e950378c4cb67429e25bcf03d8437a5e3c20
Superjomn <yanchunwei@outlook.com>
2020-05-19 08:18:09 +0000

    add dot python test

--- cinn/python/cinn/test_python_api.py
+++ cinn/python/cinn/test_python_api.py
@@ -13,7 +13,7 @@ class TestElementwiseAdd(unittest.TestCase):
 
         comp = cinn.Computation(context, "fn0")
         p0_shape = cinn.Shape()
-        p0_shape.add_int_dim(10)
+        p0_shape.add_int_dim(100)
         p0_shape.add_int_dim(20)
 
         p0 = comp.add_parameter(0, p0_shape, "p0", "float32")
@@ -29,9 +29,9 @@ class TestElementwiseAdd(unittest.TestCase):
         self.compiler.compile(module)
 
     def test1(self):
-        py_data0 = cinn.Buffer(np.float32(np.random.random((10, 20))))
-        py_data1 = cinn.Buffer(np.float32(np.random.random((10, 20))))
-        py_data2 = cinn.Buffer(np.zeros((10, 20), dtype='float32'))
+        py_data0 = cinn.Buffer(np.float32(np.random.random((100, 20))))
+        py_data1 = cinn.Buffer(np.float32(np.random.random((100, 20))))
+        py_data2 = cinn.Buffer(np.zeros((100, 20), dtype='float32'))
 
         args = cinn.Args()
         args.add_buffer(py_data0)
@@ -46,7 +46,56 @@ class TestElementwiseAdd(unittest.TestCase):
 
         self.assertTrue((np_data2 == (np_data0 + np_data1)).all())
 
-        print(py_data2.to_numpy())
+class TestDot(unittest.TestCase):
+    def setUp(self) -> None:
+        self.batch_size = 20
+        self.M = 100
+        self.N = 30
+        self.K = 50
+
+        context = cinn.Context()
+
+        comp = cinn.Computation(context, "fn0")
+        x_shape = cinn.Shape()
+        x_shape.add_var_dim("n")
+        x_shape.add_int_dim(self.M)
+        x_shape.add_int_dim(self.K)
+
+        w_shape = cinn.Shape()
+        w_shape.add_int_dim(self.K)
+        w_shape.add_int_dim(self.N)
+
+        xp = comp.add_parameter(0, x_shape, "x", "float32")
+        wp = comp.add_parameter(1, w_shape, "w", "float32")
+        dot_out = comp.add_dot(xp, wp)
+
+        module = cinn.Module("module0")
+        module.add_entry_computation(comp)
+
+        self.compiler = cinn.Compiler()
+        self.compiler.compile(module)
+
+    def test(self):
+        py_data0 = cinn.Buffer(np.float32(np.random.random((self.batch_size, self.M, self.K))))
+        py_data1 = cinn.Buffer(np.float32(np.random.random((self.K, self.N))))
+        py_data2 = cinn.Buffer(np.zeros((self.batch_size, self.M, self.N), dtype='float32'))
+
+        args = cinn.Args()
+        args.add_int32(self.batch_size)
+        args.add_buffer(py_data0)
+        args.add_buffer(py_data1)
+        args.add_buffer(py_data2)
+
+        self.compiler.eval("fn0", args)
+
+        np_data0 = py_data0.to_numpy()
+        np_data1 = py_data1.to_numpy()
+        np_data2 = py_data2.to_numpy()
+
+        target = np_data0.dot(np_data1)
+
+        self.assertTrue(np.isclose(np_data2 , target).all())
+
 
 
 if __name__ == '__main__':
