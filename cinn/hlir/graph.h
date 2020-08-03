da65a4f7abd91223b70f8d8c6a1e6425a72342b5
haozech <chenhaoze94@gmail.com>
2020-08-03 12:35:42 +0000

    fix bug and improve codestyle

--- cinn/hlir/graph.h
+++ cinn/hlir/graph.h
@@ -10,17 +10,36 @@ using cinn::utils::get;
 
 namespace cinn {
 namespace hlir {
-// computation graph
-class CINN_Graph : public cinn::common::Graph {
+
+/*!
+ * \brief Symbolic computation graph.
+ *  This is the intermediate representation for optimization pass.
+ */
+class Graph : public cinn::common::Graph {
  public:
+  /*! \brief outputs of the computation graph. */
   std::vector<NodeData*> outputs;
+
+  /*! \brief attributes of a graph */
   std::unordered_map<std::string, std::shared_ptr<any>> attrs;
+
+  /*!
+   * \brief Get the immutable attribute from attrs.
+   * \param attr_name the name of the attribute
+   * \return the reference to corresponding attribute
+   * \tparam T the type of the attribute.
+   */
   template <typename T>
   inline const T& GetAttr(const std::string& attr_name) const {
     auto it = attrs.find(attr_name);
     CHECK_NE(it, attrs.end()) << "Cannot find attribute " << attr_name << " in the graph";
     return get<T>(*it->second);
   }
+  /*!
+   * \brief Check whether has a specific attribute.
+   * \param attr_name the name of the attribute
+   * \return a boolean result
+   */
   inline bool HasAttr(const std::string& attr_name) const {
     auto it = attrs.find(attr_name);
     return it != attrs.end();
--- cinn/hlir/node.h
+++ cinn/hlir/node.h
@@ -9,27 +9,82 @@
 
 namespace cinn {
 namespace hlir {
+
 class Node;
 using NodePtr = std::shared_ptr<Node>;
-// NodeAttr represents the additional attribute of a node.
+
+/*!
+ * \brief Attributes of each node in graph.
+ *  The attributes include the node's name, the corresponding operator
+ *  and other parameters like axis.
+ */
 struct NodeAttr {
+  /*!
+   * \brief The operator this node uses.
+   */
   const Operator *op{nullptr};
+  /*!
+   * \brief The name of this node.
+   */
   std::string node_name;
+  /*!
+   * \brief The attributes stored as string in dictionary.
+   */
   std::unordered_map<std::string, std::string> attr_store;
 };
-// NodeData represents the output of each node.
+
+/*!
+ * \brief NodeData represents the output data from an operator.
+ */
 class NodeData : public cinn::common::GraphNode {
   NodeData(NodePtr node, uint32_t index, uint32_t version)
       : source_node(std::move(node)), output_index(index), version(version) {}
 
   NodeData() : source_node(), output_index(), version() {}
+
+  static std::shared_ptr<NodeData> Create(
+      const char *op_name,
+      std::string node_name,
+      std::vector<NodeData> inputs,
+      std::unordered_map<std::string, std::string> attrs = std::unordered_map<std::string, std::string>()) {
+    auto res                           = std::make_shared<NodeData>();
+    res->source_node                   = Node::Create();
+    res->source_node->attrs.op         = Operator::Get(op_name);
+    res->source_node->attrs.node_name  = std::move(node_name);
+    res->source_node->attrs.attr_store = attrs;
+    return res;
+  }
+
+  /*!
+   * \brief Get the unique id of this NodeData.
+   */
   std::string id() { return node_id; }
+  /*!
+   * \brief Source_node represents the operator this NodeData comes from.
+   */
   NodePtr source_node;
+  /*!
+   * \brief Output_index represents the index of this output data
+   *  among all the outputs of the operator.
+   *  For example, if an operator has 2 outputs, the index of
+   *  the 2 NodeData should be 0 and 1.
+   */
   uint32_t output_index;
+  /*!
+   * \brief The version of input Variable.
+   *  This field can only be nonzero when this->node is a Variable node.
+   *  version is increased by one each time a Variable get composed to a mutation Op.
+   */
   uint32_t version;
+  /*!
+   * \brief The unique id of this NodeData.
+   */
   std::string node_id;
 };
 
+/*!
+ * \brief Node represents an operation in a computation graph.
+ */
 class Node : public cinn::common::GraphNode {
  public:
   Node() = default;
@@ -38,43 +93,29 @@ class Node : public cinn::common::GraphNode {
     this->attrs.node_name = name;
   }
   ~Node();
-  std::string id() { return node_id; }
-  // The attributes of this node
-  NodeAttr attrs;
 
+  /*!
+   * \brief Get the unique id of this NodeData.
+   */
+  std::string id() { return node_id; }
+  /*!
+   * \brief The attributes in the node.
+   */
+  NodeAttr attrs;
+  /*!
+   * \brief The unique id of the node.
+   */
   std::string node_id;
 
   inline const Operator *op() const { return this->attrs.op; }
   inline bool is_variable() { return (this->attrs.op == nullptr); }
-  inline uint32_t num_outputs() {
-    if (is_variable())
-      return 1;
-    else
-      return this->op()->num_outputs;
-  }
-  inline uint32_t num_inputs() {
-    if (is_variable())
-      return 1;
-    else
-      return this->op()->num_inputs;
-  }
+  inline uint32_t num_outputs() { return is_variable() ? 1 : this->op()->num_outputs; }
+  inline uint32_t num_inputs() { return is_variable() ? 1 : this->op()->num_inputs; }
   template <class... Args>
   static NodePtr Create(Args &&... args) {
     return std::make_shared<Node>(std::forward<Args>(args)...);
   }
 };
-
-inline NodeData MakeNode(
-    const char *op_name,
-    std::string node_name,
-    std::vector<NodeData> inputs,
-    std::unordered_map<std::string, std::string> attrs = std::unordered_map<std::string, std::string>()) {
-  NodePtr p           = Node::Create();
-  p->attrs.op         = Operator::Get(op_name);
-  p->attrs.node_name  = std::move(node_name);
-  p->attrs.attr_store = attrs;
-  return NodeData(p, 0, 0);
-}
 
 }  // namespace hlir
 }  // namespace cinn
