4e30a2246955ac2489886aa132a67af0cf3d3c0f
Yan Chunwei <yanchunwei@outlook.com>
2020-08-10 19:48:51 +0800

    remove the old instruction architecure (#164)

--- cinn/hlir/instruction/schedule_impl.cc
+++
@@ -1,7 +0,0 @@
-#include "cinn/hlir/instruction/schedule_impl.h"
-
-namespace cinn {
-namespace hlir {
-namespace instruction {}  // namespace instruction
-}  // namespace hlir
-}  // namespace cinn
