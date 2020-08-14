888645a60fefce8128da1889b43890b8f4b815c8
haozech <chenhaoze94@gmail.com>
2020-08-14 03:30:34 +0000

    add strategy test

--- cinn/hlir/framework/op_strategy.h
+++ cinn/hlir/framework/op_strategy.h
@@ -62,7 +62,7 @@ class OpImpl : public common::Object {
 class OpSpec : public common::Object {
  public:
   //! List of implementations.
-  std::vector<OpImpl*> implementations;
+  std::vector<std::shared_ptr<OpImpl>> implementations;
 
   /** \brief Condition to enable the specialization.
    *    Could be undefined to represent generic case.
@@ -74,7 +74,7 @@ class OpSpec : public common::Object {
   const char* type_info() const override { return _type_key; }
 
   void AddImpl(CINNCompute fcompute, CINNSchedule fschedule, std::string name, int plevel) {
-    auto n       = make_shared<OpImpl>();
+    auto n       = std::make_shared<OpImpl>();
     n->fcompute  = fcompute;
     n->fschedule = fschedule;
     n->name      = std::move(name);
@@ -91,7 +91,7 @@ class OpStrategy : public common::Object {
  public:
   const char* type_info() const override { return "CINNOpStrategy"; }
   //! List of operator specializations.
-  std::vector<OpSpec*> specializations;
+  std::vector<std::shared_ptr<OpSpec>> specializations;
 
   /**
    * \brief Add an implementation.
@@ -103,20 +103,36 @@ class OpStrategy : public common::Object {
   void AddImpl(CINNCompute fcompute, CINNSchedule fschedule, std::string name, int plevel) {
     //! TODO(haozech) : here curr_cond should get the condition from outside.
     //! Expected : auto curr_cond = SpecializedCondition::Current();
-    std::string curr_cond = "current_condition";
-    OpSpec* op_spec;
-    for (OpSpec* op_spec : specializations) {
-      if (op_spec->condition == curr_cond) {
+    std::string curr_condition = "default";
+    for (auto op_spec : specializations) {
+      if (op_spec->condition == curr_condition) {
         op_spec->AddImpl(fcompute, fschedule, std::move(name), plevel);
         return;
       }
     }
-    OpSpec* n    = make_shared<OpSpec>();
-    n->condition = curr_cond;
+    std::shared_ptr<OpSpec> n = std::make_shared<OpSpec>();
+    n->condition              = curr_condition;
     n->AddImpl(fcompute, fschedule, std::move(name), plevel);
     this->specializations.push_back(n);
   }
 };
+
+std::shared_ptr<OpImpl> SelectImpl(std::shared_ptr<OpStrategy> strategy) {
+  //! should get the host info from global environment.
+  std::string curr_condition  = "default";
+  std::shared_ptr<OpImpl> res = nullptr;
+  for (auto spec : strategy->specializations) {
+    if (spec->condition == "default") {
+      for (auto i : spec->implementations) {
+        if (!res || res->plevel < i->plevel) {
+          res = i;
+        }
+      }
+    }
+  }
+  CHECK(res) << "There is no available strategy implementation! SelectImpl failed!";
+  return res;
+}
 
 }  // namespace framework
 }  // namespace hlir
