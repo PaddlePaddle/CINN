9d8b3cd12c4a8ec8270af2ef3f92713eac5c952e
Superjomn <yanchunwei@outlook.com>
2020-08-27 05:03:32 +0000

    add docs to frontend/syntax

--- cinn/frontend/syntax.h
+++ cinn/frontend/syntax.h
@@ -19,8 +19,16 @@ namespace frontend {
 struct Program;
 struct Variable;
 
+/**
+ * Placeholder is the fed slot of a computation.
+ */
 class Placeholder {
  public:
+  /**
+   * @param type Type of the fed
+   * @param shape Shape of the fed
+   * @param id ID of the fed
+   */
   Placeholder(const common::Type& type, const std::vector<int>& shape, std::string_view id = "")
       : type_(type), shape_(shape), id_(id.empty() ? common::Context::Global().NewName("placeholder") : id) {}
 
@@ -48,7 +56,14 @@ struct _Variable_ : public common::Object {
   static constexpr char* __type_info__ = "cinn_frontend_variable";
 };
 
+/**
+ * Variable represents the variable in a computation.
+ */
 struct Variable : public common::Shared<_Variable_> {
+  /**
+   * Constructor.
+   * @param id The identifier of the variable, if null, a random ID will be assigned.
+   */
   explicit Variable(std::string_view id = "") : common::Shared<_Variable_>(common::make_shared<_Variable_>()) {
     get()->id = id.empty() ? common::Context::Global().NewName("var") : id;
   }
@@ -57,6 +72,9 @@ struct Variable : public common::Shared<_Variable_> {
   const _Variable_* operator->() const { return get(); }
 };
 
+/**
+ * Data of a Instruction.
+ */
 struct _Instruction_ : public common::Object {
   using attr_t = std::variant<int, float, std::string, std::vector<int>, std::vector<float>, std::vector<std::string>>;
 
@@ -71,17 +89,36 @@ struct _Instruction_ : public common::Object {
   static constexpr char* __type_info__ = "cinn_frontend_instruction";
 };
 
+/**
+ * Instruction is the basic computational unit of a Program, similar to the operator concept in a DNN platform.
+ */
 struct Instruction : public common::Shared<_Instruction_> {
   explicit Instruction(std::string_view op_type, Program* parent = nullptr);
 
+  /**
+   * Set the inputs of the instruction.
+   * @param vars The input variables.
+   */
   void SetInputs(const std::vector<Variable>& vars) { get()->inputs = vars; }
   const std::vector<Variable>& GetOutputs() const { return get()->outputs; }
 
+  /**
+   * Set an attribute of the instruction.
+   * @tparam T The type of the attribute value type.
+   * @param key The identifier of the attribute.
+   * @param v The value of the attribute.
+   */
   template <typename T>
   void SetAttr(const std::string& key, const T& v) {
     get()->attrs[key] = v;
   }
 
+  /**
+   * Get an attribute of the instruction.
+   * @tparam T The data type of the attribute value.
+   * @param key The identifier of the attribute.
+   * @return The attribute value.
+   */
   template <typename T>
   T GetAttrs(const std::string& key) {
     auto it = get()->attrs.find(key);
@@ -94,6 +131,9 @@ struct Instruction : public common::Shared<_Instruction_> {
   void PrepareOutputs();
 };
 
+/**
+ * Program is a representation of a computation.
+ */
 struct Program {
   /**
    * Add two variables.
@@ -113,6 +153,10 @@ struct Program {
    */
   const Instruction& operator[](size_t i) const;
 
+  /**
+   * Get number of instructions in the program.
+   * @return
+   */
   inline size_t size() const { return instrs.size(); }
 
  private:
