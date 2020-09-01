e0c2667b5e3731e461b53c2ab4478a5654753819
Superjomn <yanchunwei@outlook.com>
2020-09-01 05:46:13 +0000

    update style

--- cinn/frontend/paddle/cpp/var_desc.h
+++ cinn/frontend/paddle/cpp/var_desc.h
@@ -2,43 +2,42 @@
 
 namespace cinn::frontend::paddle::cpp {
 
-  /*
-   * The cpp::VarDesc is the internal representation for Op. All the internal
-   * imprementation should use it, not the pb::VarDesc.
-   */
-  class VarDesc : public VarDescAPI {
+/*
+ * The cpp::VarDesc is the internal representation for Op. All the internal
+ * imprementation should use it, not the pb::VarDesc.
+ */
+class VarDesc : public VarDescAPI {
  public:
-    VarDesc() = default;
+  VarDesc() = default;
 
-    explicit VarDesc(std::string name) : name_(name) {}
+  explicit VarDesc(std::string name) : name_(name) {}
 
-    std::string Name() const override { return name_; }
+  std::string Name() const override { return name_; }
 
-    void SetName(std::string name) override { name_ = name; }
+  void SetName(std::string name) override { name_ = name; }
 
-    Type GetType() const override { return type_; }
+  Type GetType() const override { return type_; }
 
-    void SetType(Type type) override { type_ = type; }
+  void SetType(Type type) override { type_ = type; }
 
-    bool Persistable() const override { return persistable_; }
+  bool Persistable() const override { return persistable_; }
 
-    void SetPersistable(bool persistable) override { persistable_ = persistable; }
+  void SetPersistable(bool persistable) override { persistable_ = persistable; }
 
-    Type GetDataType() const { return data_type_; }
+  Type GetDataType() const { return data_type_; }
 
-    void SetDataType(Type data_type) { data_type_ = data_type; }
+  void SetDataType(Type data_type) { data_type_ = data_type; }
 
-    void SetShape(const std::vector<int64_t> &dims) { shape_ = dims; }
+  void SetShape(const std::vector<int64_t> &dims) { shape_ = dims; }
 
-    std::vector<int64_t> GetShape() const { return shape_; }
+  std::vector<int64_t> GetShape() const { return shape_; }
 
  private:
-    std::string name_;
-    Type type_;
-    Type data_type_;
-    bool persistable_;
-    std::vector<int64_t> shape_;
-  };
-
+  std::string name_;
+  Type type_;
+  Type data_type_;
+  bool persistable_;
+  std::vector<int64_t> shape_;
+};
 
 }  // namespace cinn::frontend::paddle::cpp
