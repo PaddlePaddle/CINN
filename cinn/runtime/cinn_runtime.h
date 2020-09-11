7475ca696cb1a32e5b4b5cbc19a0a61f0fe0079f
Superjomn <yanchunwei@outlook.com>
2020-09-11 02:29:52 +0000

    up

--- cinn/runtime/cinn_runtime.h
+++ cinn/runtime/cinn_runtime.h
@@ -279,10 +279,12 @@ static inline int32_t cinn_max(int32_t a, int32_t b) { return a > b ? a : b; }
 }  // extern "C"
 #endif
 
-#ifndef CINN_NOT_IMPLEMENTED
-#define CINN_NOT_IMPLEMENTED           \
-  fprintf(stderr, "Not Implemented!"); \
-  abort();
+#ifndef CINN_RUNTIME_NOT_IMPLEMENTED
+#define CINN_RUNTIME_NOT_IMPLEMENTED     \
+  do {                                   \
+    fprintf(stderr, "Not Implemented!"); \
+    abort();                             \
+  } while (false);
 #endif
 
 #define ASSERT_NOT_NULL(v__)          \
