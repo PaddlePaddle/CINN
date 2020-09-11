fix bugs and add softmax op

python/tests/test_frontend.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+import paddle as paddle
+import paddle.fluid as fluid
 from cinn.frontend import *
 from cinn import Target
 from cinn.framework import *
@@ -19,33 +21,57 @@ class TestFrontend(unittest.TestCase):
         self.target.os = Target.OS.Linux
 
     def paddle_test(self, result):
-        print(
-            "Do nothing now! Import paddle leads to segmentation fault. To be fixed."
-        )
+        a = fluid.layers.data(name='a', shape=[24, 56, 56], dtype='float32')
+        b = fluid.layers.data(name='b', shape=[24, 56, 56], dtype='float32')
+        c = fluid.layers.elementwise_add(a, b)
+        d = fluid.layers.relu(c)
+        e = fluid.initializer.NumpyArrayInitializer(
+            np.array(result[2]).reshape((144, 24, 1, 1)).astype("float32"))
+        f = fluid.layers.conv2d(
+            input=d,
+            num_filters=144,
+            filter_size=1,
+            stride=1,
+            padding=0,
+            dilation=1,
+            param_attr=e)
+        g = fluid.layers.scale(f, scale=2.0, bias=0.5)
+        res = fluid.layers.softmax(g, axis=1)
+
+        exe = fluid.Executor(fluid.CPUPlace())
+        exe.run(fluid.default_startup_program())
+
+        x = np.array(result[0]).reshape((1, 24, 56, 56)).astype("float32")
+        y = np.array(result[1]).reshape((1, 24, 56, 56)).astype("float32")
+        output = exe.run(feed={"a": x, "b": y}, fetch_list=[res])
+        output = np.array(output).reshape(-1)
+        for i in range(0, 10):
+            print(result[len(result) - 1][i], " vs: ", output[i])
+        self.assertTrue(
+            np.allclose(result[len(result) - 1], output, atol=1e-4))
 
     def test_basic(self):
         prog = Program()
 
-        a = Variable("a").set_type(Float(32)).set_shape([1, 24, 56, 56])
-        b = Variable("b").set_type(Float(32)).set_shape([1, 24, 56, 56])
+        a = Variable("A").set_type(Float(32)).set_shape([1, 24, 56, 56])
+        b = Variable("B").set_type(Float(32)).set_shape([1, 24, 56, 56])
         c = prog.add(a, b)
         d = prog.relu(c)
-        e = Variable("e").set_type(Float(32)).set_shape([144, 24, 1, 1])
-        [f1, f2, f3] = prog.conv2d(d, e, {
+        e = Variable("E").set_type(Float(32)).set_shape([144, 24, 1, 1])
+        f = prog.conv2d(d, e, {
             "stride": [1, 1],
             "dilation": 1,
             "padding": [0, 0]
         })
+        g = prog.scale(f, {"scale": 2.0, "bias": 0.5})
+        h = prog.softmax(g, {"axis": 1})
 
-        self.assertEqual(prog.size(), 3)
+        self.assertEqual(prog.size(), 5)
         # print program
         for i in range(prog.size()):
             print(prog[i])
-        result = prog.build_with_inputs(self.target, [a, b, e], f3)
-        print("The result list's size is: ")
-        print(len(result))
-        print("The output tensor's length is: ")
-        print(len(result[len(result) - 1]))
+        result = prog.build_with_inputs(self.target, [a, b, e], h)
+
         self.paddle_test(result)
 
 

python/tests/test_op_nn.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
-# import paddle as paddle
-# import paddle.fluid as fluid
+import paddle as paddle
+import paddle.fluid as fluid
 import numpy as np
 import unittest
 import math
-import numpy as np
 import cinn
 from cinn import frontend
 from cinn import runtime
@@ -39,15 +38,23 @@ class OpTest_relu6(SingleOpTester):
         self.to_test_op([[32, 32]], [[32, 32]], "relu6", attrs)
 
 
-""" class OpTest_conv2d(SingleOpTester):
+class OpTest_conv2d(SingleOpTester):
     def create_target_data(self, inputs_data, attrs):
-        img = fluid.layers.data(name='img', shape=[1,3,10,10], dtype='float32')
-        param = fluid.initializer.NumpyArrayInitializer(np.array(inputs_data[1]).reshape((2,3,2,2)).astype("float32"))
-        res = fluid.layers.conv2d(input=img, num_filters=2, filter_size=2,stride = 2, padding = 1,dilation = 2, param_attr=param)
+        img = fluid.layers.data(name='img', shape=[3, 10, 10], dtype='float32')
+        param = fluid.initializer.NumpyArrayInitializer(
+            np.array(inputs_data[1]).reshape((2, 3, 2, 2)).astype("float32"))
+        res = fluid.layers.conv2d(
+            input=img,
+            num_filters=2,
+            filter_size=2,
+            stride=2,
+            padding=1,
+            dilation=2,
+            param_attr=param)
         exe = fluid.Executor(fluid.CPUPlace())
         exe.run(fluid.default_startup_program())
 
-        x = np.array(inputs_data[0]).reshape((1,3,10,10)).astype("float32")
+        x = np.array(inputs_data[0]).reshape((1, 3, 10, 10)).astype("float32")
         output = exe.run(feed={"img": x}, fetch_list=[res])
         output = np.array(output)
         print("output's shape is:", output.shape)
@@ -60,7 +67,8 @@ class OpTest_relu6(SingleOpTester):
         attrs.set_attr("dilation", 2)
         attrs.set_attr("groups", 1)
         self.to_test_op([[1, 3, 10, 10], [2, 3, 2, 2]],
-                        [[1, 3, 12, 12], [2, 3, 3, 3], [1, 2, 5, 5]], "conv2d", attrs) """
+                        [[1, 3, 12, 12], [2, 3, 3, 3], [1, 2, 5, 5]], "conv2d",
+                        attrs)
 
 
 class OpTest_batchnorm(SingleOpTester):
@@ -76,6 +84,21 @@ class OpTest_batchnorm(SingleOpTester):
         attrs = framework.NodeAttr()
         self.to_test_op([[1, 3, 2, 2], [4, 3]], [[1, 3, 2, 2]], "batchnorm",
                         attrs)
+
+
+class OpTest_softmax(SingleOpTester):
+    def create_target_data(self, inputs_data, attrs):
+        [X] = inputs_data
+        Y = np.zeros(X.shape).astype("float32")
+        for i in range(0, Y.shape[1]):
+            Y[:, i, :] = np.exp(X[:, i, :]) / np.sum(
+                np.exp(X), axis=1, keepdims=True)[:, 0, :]
+        return Y
+
+    def test_op(self):
+        attrs = framework.NodeAttr()
+        attrs.set_attr("axis", 1)
+        self.to_test_op([[2, 3, 4]], [[2, 3, 4], [2, 3, 4]], "softmax", attrs)
 
 
 if __name__ == "__main__":