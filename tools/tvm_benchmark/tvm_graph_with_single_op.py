0aded8ac8b83e45a504d70b1354afd7f3c6a92ad
haozech <chenhaoze94@gmail.com>
2020-11-11 05:21:09 +0000

    refine tvm test

--- tools/tvm_benchmark/tvm_graph_with_single_op.py
+++ tools/tvm_benchmark/tvm_graph_with_single_op.py
@@ -11,15 +11,17 @@ from tvm.autotvm.tuner import XGBTuner, GATuner, RandomTuner, GridSearchTuner
 from tvm.contrib.utils import tempdir
 import tvm.contrib.graph_runtime as runtime
 
-
 # To test different ops, change this single-op network.
 # See ./relay_op.rst to get the op list.
-def get_network():
+
+
+def get_network_conv2d():
     input_shape = [(1, 512, 7, 7), (512, 512, 3, 3)]
     output_shape = (1, 512, 7, 7)
     input_names = ["x", "y"]
     x = relay.Var(input_names[0], tvm.relay.TensorType(input_shape[0]))
     y = relay.Var(input_names[1], tvm.relay.TensorType(input_shape[1]))
+    print("[Test]Begin building graph with op relay.nn.conv2d")
     mod = relay.Function([x, y],
                          relay.nn.conv2d(
                              x,
@@ -31,17 +33,93 @@ def get_network():
     return mod, params, input_shape, output_shape, input_names
 
 
+def get_network_relu():
+    input_shape = [(1, 512, 7, 7)]
+    output_shape = (1, 512, 7, 7)
+    input_names = ["x"]
+    x = relay.Var(input_names[0], tvm.relay.TensorType(input_shape[0]))
+    print("[Test]Begin building graph with op relay.nn.relu")
+    mod = relay.Function([x], relay.nn.relu(x))
+    params = []
+    return mod, params, input_shape, output_shape, input_names
+
+
+def get_network_elementwise():
+    input_shape = [(1, 512, 7, 7), (1, 512, 7, 7)]
+    output_shape = (1, 512, 7, 7)
+    input_names = ["x", "y"]
+    x = relay.Var(input_names[0], tvm.relay.TensorType(input_shape[0]))
+    y = relay.Var(input_names[1], tvm.relay.TensorType(input_shape[1]))
+    print("[Test]Begin building graph with op relay.multiply")
+    mod = relay.Function([x, y], relay.multiply(x, y))
+    params = []
+    return mod, params, input_shape, output_shape, input_names
+
+
+def get_network_matmul():
+    input_shape = [(512, 512), (512, 512)]
+    output_shape = (512, 512)
+    input_names = ["x", "y"]
+    x = relay.Var(input_names[0], tvm.relay.TensorType(input_shape[0]))
+    y = relay.Var(input_names[1], tvm.relay.TensorType(input_shape[1]))
+    print("[Test]Begin building graph with op relay.nn.dense (matmul)")
+    mod = relay.Function([x, y], relay.nn.dense(x, y))
+    params = []
+    return mod, params, input_shape, output_shape, input_names
+
+
+def get_network_softmax():
+    input_shape = [(1024, 2048)]
+    output_shape = (1024, 2048)
+    input_names = ["x"]
+    x = relay.Var(input_names[0], tvm.relay.TensorType(input_shape[0]))
+    print("[Test]Begin building graph with op relay.nn.softmax")
+    mod = relay.Function([x], relay.nn.softmax(x))
+    params = []
+    return mod, params, input_shape, output_shape, input_names
+
+
+def get_network_pool2d():
+    input_shape = [(1, 64, 112, 112)]
+    output_shape = (1, 64, 56, 56)
+    input_names = ["x"]
+    x = relay.Var(input_names[0], tvm.relay.TensorType(input_shape[0]))
+    print("[Test]Begin building graph with op relay.nn.max_pool2d")
+    mod = relay.Function([x],
+                         relay.nn.max_pool2d(
+                             x,
+                             pool_size=(3, 3),
+                             strides=(2, 2),
+                             padding=(1, 1)))
+    params = []
+    return mod, params, input_shape, output_shape, input_names
+
+
+def get_network_batchnorm():
+    data0 = relay.var("data0", relay.TensorType((1, 512, 7, 7), "float32"))
+    bn_gamma = relay.var("bn_gamma1", relay.TensorType((512, ), "float32"))
+    bn_beta = relay.var("bn_beta1", relay.TensorType((512, ), "float32"))
+    bn_mmean = relay.var("bn_mean1", relay.TensorType((512, ), "float32"))
+    bn_mvar = relay.var("bn_var1", relay.TensorType((512, ), "float32"))
+    bn = relay.nn.batch_norm(data0, bn_gamma, bn_beta, bn_mmean, bn_mvar)[0]
+    input_shape = [(1, 512, 7, 7), (512), (512), (512), (512)]
+    output_shape = (1, 512, 7, 7)
+    input_names = ["data0", "bn_gamma1", "bn_beta1", "bn_mean1", "bn_var1"]
+    print("[Test]Begin building graph with op relay.nn.batch_norm")
+    mod = relay.Function([data0, bn_gamma, bn_beta, bn_mmean, bn_mvar], bn)
+    params = []
+    return mod, params, input_shape, output_shape, input_names
+
+
 #### DEVICE CONFIG ####
 target = tvm.target.cuda()
 dtype = "float32"
 
 
-def tune_and_evaluate():
+def tune_and_evaluate(func):
     # extract workloads from relay program
-    print("Extract tasks...")
-    mod, params, input_shape, out_shape, input_names = get_network()
+    mod, params, input_shape, out_shape, input_names = func()
 
-    print("Compile...")
     lib = relay.build_module.build(mod, target=target, params=params)
 
     # load parameters
@@ -68,4 +146,10 @@ def tune_and_evaluate():
           (np.mean(prof_res2), np.std(prof_res2)))
 
 
-tune_and_evaluate()
+tune_and_evaluate(get_network_conv2d)
+tune_and_evaluate(get_network_pool2d)
+tune_and_evaluate(get_network_softmax)
+tune_and_evaluate(get_network_matmul)
+tune_and_evaluate(get_network_batchnorm)
+tune_and_evaluate(get_network_relu)
+tune_and_evaluate(get_network_elementwise)
