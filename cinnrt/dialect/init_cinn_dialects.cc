ce70b287a1a928f42fd45fb571329dfe7e78d38f
Yan Chunwei <yanchunwei@outlook.com>
2020-12-25 18:07:26 +0800

    Initialize Paddle MLIR Dialect (#308)

--- cinnrt/dialect/init_cinn_dialects.cc
+++ cinnrt/dialect/init_cinn_dialects.cc
@@ -5,12 +5,13 @@
 #include "cinnrt/dialect/basic_kernels.h"
 #include "cinnrt/dialect/cinn_base.h"
 #include "cinnrt/dialect/dense_tensor.h"
+#include "cinnrt/dialect/pd_ops.h"
 #include "cinnrt/dialect/tensor_shape.h"
 
 namespace cinnrt {
 
 void RegisterCinnDialects(mlir::DialectRegistry& registry) {
-  registry.insert<ts::TensorShapeDialect, dialect::CINNDialect, dt::DTDialect>();
+  registry.insert<ts::TensorShapeDialect, dialect::CINNDialect, dt::DTDialect, mlir::PD::PaddleDialect>();
 }
 
 }  // namespace cinnrt
---
+++ cinnrt/dialect/pd_ops.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "mlir/Dialect/Traits.h"
+#include "mlir/IR/Attributes.h"
+#include "mlir/IR/Builders.h"
+#include "mlir/IR/Dialect.h"
+#include "mlir/IR/Function.h"
+#include "mlir/IR/Matchers.h"
+#include "mlir/IR/Module.h"
+#include "mlir/IR/OpImplementation.h"
+#include "mlir/IR/StandardTypes.h"
+#include "mlir/IR/TypeUtilities.h"
+#include "mlir/Interfaces/CallInterfaces.h"
+#include "mlir/Interfaces/DerivedAttributeOpInterface.h"
+#include "mlir/Interfaces/InferTypeOpInterface.h"
+#include "mlir/Interfaces/LoopLikeInterface.h"
+#include "mlir/Interfaces/SideEffectInterfaces.h"
+
+namespace mlir {
+namespace PD {
+
+class PaddleDialect : public Dialect {
+ public:
+  explicit PaddleDialect(MLIRContext* context);
+
+  static StringRef getDialectNamespace() { return "PD"; }
+
+  Type parseType(DialectAsmParser& parser) const override { return Dialect::parseType(parser); }
+  void printType(Type type, DialectAsmPrinter& printer) const override { Dialect::printType(type, printer); }
+};
+
+}  // namespace PD
+}  // namespace mlir
