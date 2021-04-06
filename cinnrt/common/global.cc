76ce32cf08fc711fb6e20313615a33ae5b870323
Tao Shibo <taoshibo@baidu.com>
2021-04-06 08:35:42 +0000

    can load_tensors from param file into TensorMap, and get_tensor from TensorMap.

--- cinnrt/common/global.cc
+++ cinnrt/common/global.cc
@@ -5,12 +5,20 @@ namespace cinnrt {
 Global::Global() {}
 
 mlir::MLIRContext* Global::context = nullptr;
+TensorMap* Global::tensorMap       = nullptr;
 
 mlir::MLIRContext* Global::getMLIRContext() {
   if (nullptr == context) {
     context = new mlir::MLIRContext();
   }
   return context;
+}
+
+TensorMap* Global::getTensorMap() {
+  if (nullptr == tensorMap) {
+    tensorMap = new TensorMap();
+  }
+  return tensorMap;
 }
 
 }  // namespace cinnrt
