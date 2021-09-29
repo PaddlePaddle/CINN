ace727cda6be7fad5228c6387050fc9e33d5866e
Zhang Ting <Douyaer2020@qq.com>
2021-09-29 12:42:05 +0800

    add decomposer registry (#458)
    * add decomposer registry
    
    Co-authored-by: Yan Chunwei <yanchunwei@outlook.com>

---
+++ cinn/frontend/decomposer/activation.cc
@@ -0,0 +1,17 @@
+#include "cinn/frontend/decomposer_registry.h"
+
+namespace cinn {
+namespace frontend {
+namespace decomposer {
+
+void relu(const Instruction& instr, const DecomposerContext& context) { LOG(FATAL) << "not implemented"; }
+
+}  // namespace decomposer
+}  // namespace frontend
+}  // namespace cinn
+
+CINN_REGISTER_HELPER(activation) {
+  CINN_DECOMPOSER_REGISTER(relu, ::cinn::common::DefaultHostTarget()).set_body(cinn::frontend::decomposer::relu);
+
+  return true;
+}
