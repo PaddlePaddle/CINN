37b5b9541d3a15c8ad05225268876a2815e4b715
jiangcheng <thisjiang@qq.com>
2021-09-29 07:24:44 +0000

    add single test script

---
+++ cinn/frontend/op_mapper_registry_test.cc
@@ -0,0 +1,21 @@
+#include "cinn/frontend/op_mapper_registry.h"
+
+#include <gtest/gtest.h>
+
+#include <typeinfo>
+
+#include "cinn/frontend/op_mappers/use_op_mappers.h"
+#include "cinn/utils/registry.h"
+
+namespace cinn {
+namespace frontend {
+
+TEST(OpMapperRegistryTest, basic) {
+  auto kernel = OpMapperRegistry::Global()->Find("sigmoid");
+  ASSERT_NE(kernel, nullptr);
+  ASSERT_EQ(typeid(*kernel), typeid(OpMapperKernel));
+  ASSERT_EQ(kernel->name, "sigmoid");
+}
+
+}  // namespace frontend
+}  // namespace cinn
