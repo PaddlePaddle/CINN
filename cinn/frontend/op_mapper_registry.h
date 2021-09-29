3fbe680fd31b3a4062c612fe36607248efbbebe1
jiangcheng <thisjiang@qq.com>
2021-09-29 02:45:28 +0000

    ad op mapper and add sigmoid register example

---
+++ cinn/frontend/op_mapper_registry.h
@@ -0,0 +1,74 @@
+#pragma once
+
+#include <functional>
+#include <memory>
+#include <string>
+#include <unordered_map>
+#include <variant>
+
+#include "cinn/common/macros.h"
+#include "cinn/common/target.h"
+#include "cinn/frontend/net_builder.h"
+#include "cinn/frontend/paddle/cpp/op_desc.h"
+#include "cinn/frontend/syntax.h"
+#include "cinn/hlir/framework/scope.h"
+#include "cinn/utils/registry.h"
+
+namespace cinn {
+namespace frontend {
+
+class OpMapperContext {
+ public:
+  OpMapperContext(hlir::framework::Scope* scope,
+                  const common::Target& target,
+                  NetBuilder* builder,
+                  absl::flat_hash_map<std::string, Variable>* var_map,
+                  absl::flat_hash_map<std::string, std::string>* var_model_to_program_map)
+      : scope_(scope),
+        target_(target),
+        builder_(builder),
+        var_map_(var_map),
+        var_model_to_program_map_(var_model_to_program_map) {}
+
+  hlir::framework::Scope* scope_{nullptr};
+  const common::Target& target_;
+  NetBuilder* builder_{nullptr};
+  absl::flat_hash_map<std::string, Variable>* var_map_{nullptr};
+  // map from var in Paddle model to var name in program.
+  absl::flat_hash_map<std::string, std::string>* var_model_to_program_map_{nullptr};
+};
+
+class OpMapperKernel {
+ public:
+  using KernelFunc = std::function<void(const paddle::cpp::OpDesc&, const OpMapperContext&)>;
+
+  OpMapperKernel() = default;
+
+  OpMapperKernel& Set(const KernelFunc& kernel) {
+    kernel_ = kernel;
+    return *this;
+  }
+  void Run(const paddle::cpp::OpDesc& op_desc, const OpMapperContext& ctx) const { kernel_(op_desc, ctx); }
+
+  std::string name;
+
+ private:
+  KernelFunc kernel_;
+};
+
+class OpMapperRegistry : public Registry<OpMapperKernel> {
+ public:
+  OpMapperRegistry() = default;
+
+ private:
+  CINN_DISALLOW_COPY_AND_ASSIGN(OpMapperRegistry);
+};
+
+#define UNIQUE_OPMAPPER_NAME(OpName) static ::cinn::frontend::OpMapperKernel& __op_mapper_registrar_##OpName
+
+#define CINN_REGISTER_OPMAPPER(OpName, Kernel)                 \
+  CINN_STR_CONCAT(UNIQUE_OPMAPPER_NAME(OpName), __COUNTER__) = \
+      ::cinn::frontend::OpMapperRegistry::Global()->__REGISTER_OR_GET__(#OpName).Set(Kernel);
+
+}  // namespace frontend
+}  // namespace cinn
---
+++ cinn/frontend/op_mappers/sigmoid.cc
@@ -0,0 +1,25 @@
+#include "cinn/frontend/op_mapper_registry.h"
+#include "cinn/frontend/op_mappers/common_utils.h"
+
+namespace cinn {
+namespace frontend {
+namespace op_mappers {
+
+void sigmoid_kernel(const paddle::cpp::OpDesc& op_desc, const OpMapperContext& ctx) {
+  CHECK_EQ(op_desc.Input("X").size(), 1UL);
+  auto x_name = op_desc.Input("X").front();
+  CHECK_EQ(op_desc.Output("Out").size(), 1UL);
+  auto out_name = op_desc.Output("Out").front();
+
+  auto x   = utils::GetVar(cinn::utils::TransValidVarName(x_name), ctx);
+  auto out = ctx.builder_->sigmoid(x);
+
+  utils::AddVar(cinn::utils::TransValidVarName(out_name), out, ctx);
+  (*ctx.var_model_to_program_map_)[out_name] = out->id;
+}
+
+}  // namespace op_mappers
+}  // namespace frontend
+}  // namespace cinn
+
+CINN_REGISTER_HELPER(sigmoid) { CINN_REGISTER_OPMAPPER(sigmoid, cinn::frontend::op_mappers::sigmoid_kernel) }
