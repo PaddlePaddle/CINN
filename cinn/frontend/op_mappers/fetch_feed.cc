bbef7290100c0b649a3eae43e3216aebbc0cb213
jiangcheng <thisjiang@qq.com>
2021-10-09 14:07:04 +0800

    Full op_mappers by `CINN_REGISTER_OPMAPPER` (#460)
    * full op_mapper's op and kernels
    
    * add mulbias and add opmapper
    
    * fix some problem by review advices and CINN_REGISTER_OP_MAPPER
    
    * fix CI bug: AddVar in mul.cc should has utils namespace
    
    * optimize AddVar and GetVar prototype
    
    * add AddVarModelToProgramMap instead of directly access var_model_to_program_map_
    
    * change name from kernel to OpMaker
    
    * change name from OpMaker to OpMapper
    
    Co-authored-by: Yan Chunwei <yanchunwei@outlook.com>

---
+++ cinn/frontend/op_mappers/fetch_feed.cc
@@ -0,0 +1,30 @@
+#include "cinn/common/macros.h"
+#include "cinn/frontend/op_mapper_registry.h"
+#include "cinn/frontend/op_mappers/common_utils.h"
+
+namespace cinn {
+namespace frontend {
+namespace op_mappers {
+
+void FetchOpMapper(const paddle::cpp::OpDesc& op_desc, const OpMapperContext& ctx) {
+  CHECK_EQ(op_desc.Input("X").size(), 1UL);
+  auto output_name = op_desc.Input("X").front();
+  LOG(INFO) << "detect model output: [" << output_name << "]";
+}
+
+void FeedOpMapper(const paddle::cpp::OpDesc& op_desc, const OpMapperContext& ctx) {
+  auto outs = op_desc.Output("Out");
+  CHECK_EQ(outs.size(), 1UL);
+  VLOG(2) << "Model get feed [" << outs[0] << "]";
+  Placeholder input(common::Float(32), {}, outs[0]);
+  ctx.AddVar(outs[0], input);
+}
+
+}  // namespace op_mappers
+}  // namespace frontend
+}  // namespace cinn
+
+CINN_REGISTER_HELPER(fetch_feed) {
+  CINN_REGISTER_OP_MAPPER(fetch, cinn::frontend::op_mappers::FetchOpMapper)
+  CINN_REGISTER_OP_MAPPER(feed, cinn::frontend::op_mappers::FeedOpMapper)
+}
--- cinn/frontend/op_mappers/softmax.cc
+++ cinn/frontend/op_mappers/softmax.cc
@@ -5,15 +5,17 @@ namespace cinn {
 namespace frontend {
 namespace op_mappers {
 
-void SigmoidOpMapper(const paddle::cpp::OpDesc& op_desc, const OpMapperContext& ctx) {
+void SoftmaxOpMapper(const paddle::cpp::OpDesc& op_desc, const OpMapperContext& ctx) {
   CHECK_EQ(op_desc.Input("X").size(), 1UL);
   auto x_name = op_desc.Input("X").front();
   CHECK_EQ(op_desc.Output("Out").size(), 1UL);
   auto out_name = op_desc.Output("Out").front();
 
-  auto x   = ctx.GetVar(x_name);
-  auto out = ctx.builder_->sigmoid(x);
+  auto axis        = utils::GetAttrOrDefault<int>(op_desc, "axis", -1);
+  auto data_format = utils::GetAttrOrDefault<std::string>(op_desc, "data_format", "AnyLayout");
 
+  auto x   = ctx.GetVar(x_name);
+  auto out = ctx.builder_->softmax(x, axis, data_format);
   ctx.AddVar(out_name, out);
   ctx.AddVarModelToProgramMap(out_name, out->id);
 }
@@ -22,4 +24,4 @@ void SigmoidOpMapper(const paddle::cpp::OpDesc& op_desc, const OpMapperContext& 
 }  // namespace frontend
 }  // namespace cinn
 
-CINN_REGISTER_HELPER(sigmoid) { CINN_REGISTER_OP_MAPPER(sigmoid, cinn::frontend::op_mappers::SigmoidOpMapper) }
+CINN_REGISTER_HELPER(softmax) { CINN_REGISTER_OP_MAPPER(softmax, cinn::frontend::op_mappers::SoftmaxOpMapper) }
