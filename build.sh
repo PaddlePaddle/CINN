ed4f1c94b8b8c917459969291e21e557b893f24d
Superjomn <yanchunwei@outlook.com>
2021-10-11 07:26:40 +0000

    up

--- build.sh
+++ build.sh
@@ -147,7 +147,8 @@ function prepare_model {
 
 function codestyle_check {
     pre-commit run -a
-    if [[ git diff-index --quite HEAD ]]; then
+    if ! git diff-index --quiet HEAD --; then
+
         echo "Code is dirty, please run 'pre-commit run -a' to reformat the code changes"
         exit -1
     fi
