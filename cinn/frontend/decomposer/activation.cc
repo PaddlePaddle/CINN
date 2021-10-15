cf9fa4f38b1d0787ad2c0ce61b44625ba3d9d085
Zhang Ting <zhangting_2017@163.com>
2021-10-15 21:20:41 +0800

    add relu_grad decomposer (#479)

--- cinn/frontend/decomposer/activation.cc
+++ cinn/frontend/decomposer/activation.cc
@@ -34,12 +34,35 @@ void relu(const Instruction& instr, const DecomposerContext& context) {
   context.MapVarToOrigin(out, output);
 }
 
+void relu_grad(const Instruction& instr, const DecomposerContext& context) {
+  CHECK_EQ(instr->inputs.size(), 2UL) << " 2 input tensors for " << instr->op_type;
+  CHECK_EQ(instr->outputs.size(), 1UL) << "1 output tensor for " << instr->op_type;
+  auto dout    = instr->inputs[0];
+  auto out     = instr->inputs[1];
+  auto dx      = instr->outputs[0];
+  auto builder = context.builder_;
+
+  auto zero_var   = builder->ConstScalar<float>(0.f, common::UniqName("zero"));
+  auto bcast_zero = builder->BroadcastTo(zero_var, out->shape, {0});
+  auto condition  = builder->Compare(out, bcast_zero, ComparisonKind::kGt);
+  auto res        = builder->Select(condition, dout, bcast_zero);
+
+  // map the the output of decomposed operator to the original.
+  context.MapVarToOrigin(res, dx);
+}
+
 }  // namespace decomposer
 }  // namespace frontend
 }  // namespace cinn
 
 CINN_REGISTER_HELPER(activation) {
   CINN_DECOMPOSER_REGISTER(relu, cinn::frontend::decomposer::relu);
+
+  return true;
+}
+
+CINN_REGISTER_HELPER(activation_grad) {
+  CINN_DECOMPOSER_REGISTER(relu_grad, cinn::frontend::decomposer::relu_grad);
 
   return true;
 }
