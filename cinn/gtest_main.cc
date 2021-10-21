371cb57781712e0972a4d5955d4887db95e6d056
hp03 <wanghaipeng03@baidu.com>
2021-10-21 06:19:32 +0000

    fix problems to let CINN work with Paddle

--- cinn/gtest_main.cc
+++ cinn/gtest_main.cc
@@ -17,7 +17,7 @@
 
 int main(int argc, char **argv) {
   testing::InitGoogleTest(&argc, argv);
-  google::ParseCommandLineFlags(&argc, &argv, false);
+  GFLAGS_NAMESPACE::ParseCommandLineFlags(&argc, &argv, false);
 
   return RUN_ALL_TESTS();
 }
