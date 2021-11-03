440288c380848702c998eb56eb383bf84628ee0b
zhhsplendid <zhhsplendid@163.com>
2021-11-03 09:33:59 +0000

    Remove useless function

--- cinn/frontend/op_mappers/conv2d.cc
+++ cinn/frontend/op_mappers/conv2d.cc
@@ -20,50 +20,6 @@ namespace cinn {
 namespace frontend {
 namespace op_mappers {
 
-inline void ReverseHWData(float* data, std::vector<int> shape) {
-  CHECK_EQ(shape.size(), 4UL);
-  for (int i = 0; i < shape[0] * shape[1]; i++) {
-    int num = shape[2] * shape[3];
-    std::reverse(data + (i * num), data + (i * num + num));
-  }
-}
-
-void ReverseHWVar(const std::string& origin_name, const OpMapperContext& ctx) {
-  const auto& name = cinn::utils::TransValidVarName(origin_name);
-  CheckVarNameValid(name);
-  auto* var = ctx.Scope().FindVar(name);
-  if (var) {
-    auto& tensor = absl::get<hlir::framework::Tensor>(*var);
-    if (ctx.Target().arch == Target::Arch::X86) {
-      float* data = tensor->mutable_data<float>(ctx.Target());
-      CHECK_EQ(tensor->shape().size(), 4UL)
-          << "The y data's shape size of op [conv2d] is not equal to 4! Please check.";
-      ReverseHWData(data, tensor->shape().data());
-    } else if (ctx.Target().arch == Target::Arch::NVGPU) {
-#ifdef CINN_WITH_CUDA
-      std::vector<float> data(tensor->shape().numel());
-      CUDA_CALL(cudaMemcpy(data.data(),
-                           reinterpret_cast<void*>(tensor->mutable_data<float>(ctx.Target())),
-                           tensor->shape().numel() * sizeof(float),
-                           cudaMemcpyDeviceToHost));
-      CHECK_EQ(tensor->shape().size(), 4UL)
-          << "The y data's shape size of op [conv2d] is not equal to 4! Please check.";
-      ReverseHWData(data.data(), tensor->shape().data());
-      CUDA_CALL(cudaMemcpy(reinterpret_cast<void*>(tensor->mutable_data<float>(ctx.Target())),
-                           data.data(),
-                           tensor->shape().numel() * sizeof(float),
-                           cudaMemcpyHostToDevice));
-#else
-      LOG(FATAL) << "To use CUDA backends, you need to set WITH_CUDA ON!";
-#endif
-    } else {
-      CINN_NOT_IMPLEMENTED
-    }
-  } else {
-    LOG(FATAL) << "No var called [" << name << "] exists";
-  }
-}
-
 void Conv2dOpMapper(const paddle::cpp::OpDesc& op_desc, const OpMapperContext& ctx) {
   CHECK_EQ(op_desc.Input("Input").size(), 1UL);
   auto x_name = op_desc.Input("Input").front();
