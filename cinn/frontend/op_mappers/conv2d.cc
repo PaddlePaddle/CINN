65991f0a0a088c00a9a417b08eb84892b10b0761
sunli <466530738@qq.com>
2021-11-08 02:28:24 +0000

    Add Conv2d Grad Op Mapper

--- cinn/frontend/op_mappers/conv2d.cc
+++ cinn/frontend/op_mappers/conv2d.cc
@@ -82,6 +82,48 @@ void DepthwiseConv2dOpMapper(const paddle::cpp::OpDesc& op_desc, const OpMapperC
   ctx.AddVarModelToProgram(out_name, out->id);
 }
 
+void Conv2dGradOpMapper(const paddle::cpp::OpDesc& op_desc, const OpMapperContext& ctx) {
+  // get dy
+  CHECK_EQ(op_desc.Input(paddle::GradVarName("Output")).size(), 1UL);
+  auto dy_name = op_desc.Input(paddle::GradVarName("Output")).front();
+
+  // get intput input,filter
+  CHECK_EQ(op_desc.Input("Input").size(), 1UL);
+  auto x_name = op_desc.Input("Input").front();
+  CHECK_EQ(op_desc.Input("Filter").size(), 1UL);
+  auto w_name = op_desc.Input("Filter").front();
+
+  // get dx,dfilter
+  CHECK_EQ(op_desc.Output(paddle::GradVarName("Input")).size(), 1UL);
+  auto dx_name = op_desc.Output(paddle::GradVarName("Input")).front();
+
+  CHECK_EQ(op_desc.Output(paddle::GradVarName("Filter")).size(), 1UL);
+  auto dw_name = op_desc.Output(paddle::GradVarName("Filter")).front();
+
+  auto strides   = utils::GetAttrOrDefault<std::vector<int>>(op_desc, "strides", {1, 1});
+  auto paddings  = utils::GetAttrOrDefault<std::vector<int>>(op_desc, "paddings", {0, 0});
+  auto dilations = utils::GetAttrOrDefault<std::vector<int>>(op_desc, "dilations", {1, 1});
+  auto groups    = utils::GetAttrOrDefault<int>(op_desc, "groups", 1);
+
+  auto data_format = utils::GetAttrOrDefault<std::string>(op_desc, "data_format", "AnyLayout");
+  if (data_format == "AnyLayout") {
+    data_format = "NCHW";
+  }
+
+  auto padding_algorithm = utils::GetAttrOrDefault<std::string>(op_desc, "padding_algorithm", "EXPLICIT");
+
+  auto dy     = ctx.GetVar(dy_name);
+  auto x      = ctx.GetVar(x_name);
+  auto weight = ctx.GetVar(w_name);
+
+  auto out =
+      ctx.Builder()->conv2d_grad(dy, x, weight, strides, paddings, dilations, groups, data_format, padding_algorithm);
+  ctx.AddVar(dx_name, out[0]);
+  ctx.AddVarModelToProgram(dx_name, out[0]->id);
+  ctx.AddVar(dw_name, out[1]);
+  ctx.AddVarModelToProgram(dw_name, out[1]->id);
+}
+
 }  // namespace op_mappers
 }  // namespace frontend
 }  // namespace cinn
@@ -89,5 +131,6 @@ void DepthwiseConv2dOpMapper(const paddle::cpp::OpDesc& op_desc, const OpMapperC
 CINN_REGISTER_HELPER(conv2d) {
   CINN_REGISTER_OP_MAPPER(conv2d, cinn::frontend::op_mappers::Conv2dOpMapper)
   CINN_REGISTER_OP_MAPPER(depthwise_conv2d, cinn::frontend::op_mappers::DepthwiseConv2dOpMapper)
+  CINN_REGISTER_OP_MAPPER(conv2d_grad, cinn::frontend::op_mappers::Conv2dGradOpMapper)
   return true;
 }
