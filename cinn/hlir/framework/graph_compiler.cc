4dc07e1c70d881a977b40329d6e67369a37d5b27
CtfGo <ctfeng66@163.com>
2021-11-30 11:02:54 +0800

    add Finalize method on runtime instruction (#595)
    * add Finalize method on runtime instruction
    
    * fix instruction_test
    
    * move Finalize to the tail of BuildInstructions
    
    * fix compile
    
    * finalize in the tail of Build
    
    * adjust Finalize function used position
    
    * call Finalize along with assignment done

--- cinn/hlir/framework/graph_compiler.cc
+++ cinn/hlir/framework/graph_compiler.cc
@@ -858,6 +858,8 @@ std::vector<std::unique_ptr<Instruction>> GraphCompiler::BuildInstructions() {
       if (node->attrs.attr_store.count("pre_run")) {
         instr->pre_run = absl::get<bool>(node->attrs.attr_store["pre_run"]);
       }
+      // explicitly call Finalize of the instruction after all assignments on it were done
+      instr->Finalize();
       instructions.push_back(std::move(instr));
     } else {
       CHECK_GT(group.size(), 1U) << "fuse number should be greater than 1";
@@ -917,6 +919,8 @@ std::vector<std::unique_ptr<Instruction>> GraphCompiler::BuildInstructions() {
           instr->pre_run = true;
         }
       }
+      // explicitly call Finalize of the instruction after all assignments on it were done
+      instr->Finalize();
       instructions.push_back(std::move(instr));
     }
   }
