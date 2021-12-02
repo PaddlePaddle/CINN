369fafdf3713b5e8bf09e28959d2f3d1084b7381
Haipeng Wang <wanghaipeng03@baidu.com>
2021-12-02 15:59:53 +0800

    export CINN python wheel package (#614)

--- python/cinn/version/__init__.py
+++ python/cinn/version/__init__.py
@@ -12,17 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
-cinndir = os.path.dirname(os.path.abspath(__file__))
-runtime_include_dir = os.path.join(cinndir, "libs")
-cuhfile = os.path.join(runtime_include_dir, "cinn_cuda_runtime_source.cuh")
-
-if os.path.exists(cuhfile):
-    os.environ['runtime_include_dir'] = runtime_include_dir
-
-from .core_api.common import *
-from .core_api.backends import *
-from .core_api.poly import *
-from .core_api.ir import *
-from .core_api.lang import *
-from .version import full_version as __version__
+try:
+    from .info import *
+except:
+    full_version = 'Unknown'
