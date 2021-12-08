3cef30467e00ca1da5b5af6e0e12819a4238cc68
jiangcheng <thisjiang@qq.com>
2021-12-08 03:04:15 +0000

    perfect tutorial content

--- tutorials/cinn_builder.py
+++ tutorials/cinn_builder.py
@@ -26,27 +26,28 @@ import numpy as np
 ##################################################################
 #
 # Define the CinnBuilder.
-# ----------------
+# -----------------------------
 # Using CinnBuilder is a convenient way to build a model in CINN. You can build and run
 # a model by invoking CinnBuilder's API as following.
 #
-# :code:`name` the ID of CinnBuilder
-builder = frontend.CinnBuilder(name="add_conv2d")
+# :code:`name`: the ID of CinnBuilder
+builder = frontend.CinnBuilder(name="add_conv")
 
 ##################################################################
 #
 # Define the input variable of the model.
-# ----------------
+# ---------------------------------------------
 # The input variable should be created by create_input API. Note that the variable
 # here is just a placeholder, does not need the actual data.
 #
-# :code:`type` the data type of input variable, now support Void, Int, UInt, Float,
-# Bool and String, the parameter is the type's bit-widths.
+# :code:`type`: the data type of input variable, now support `Void`, `Int`, `UInt`,
+# `Float`, `Bool` and `String`, the parameter is the type's bit-widths, here the
+# data type is `float32`.
 #
-# :code:`shape` The shape of the input variable, note that here does not support
+# :code:`shape`: The shape of the input variable, note that here does not support
 # dynamic shape, so the dimension value should be greater than 0 now.
 #
-# :code:`id_hint` the name of variable, the defaule value is `""`
+# :code:`id_hint`: the name of variable, the defaule value is `""`
 a = builder.create_input(
     type=common.Float(32), shape=(1, 24, 56, 56), id_hint="A")
 b = builder.create_input(
@@ -57,7 +58,7 @@ c = builder.create_input(
 ##################################################################
 #
 # Build the model by using CinnBuilder API
-# ----------------
+# ---------------------------------------------
 # For convenience, here we build a simple model that only consists of add and conv
 # operators. Note that you can find the operator's detailed introduction in another
 # document, we won't go into detail here.
@@ -70,7 +71,7 @@ e = builder.conv(d, c)
 ##################################################################
 #
 # Generate the program
-# ----------------
+# ---------------------
 # After the model building, the CinnBuilder will generate a CINN execution program,
 # and you can get it by invoking `builder.build()` function.
 prog = builder.build()
@@ -82,7 +83,7 @@ for i in range(prog.size()):
 ##################################################################
 #
 # Random fake input data
-# ----------------
+# -----------------------------
 # Before running, you should read or generate some data to feed the model's input.
 # In model building, we just create some placeholder, to get the model's running
 # result, here we random some fake input data.
@@ -95,30 +96,30 @@ tensor_data = [
 ##################################################################
 #
 # Set target
-# ----------------
+# ---------------------
 # The target identified where the model should run, now we support
 # two targets:
 #
-# :code:`DefaultHostTarget` the model will running at cpu.
+# :code:`DefaultHostTarget`: the model will running at cpu.
 #
-# :code:`DefaultNVGPUTarget` the model will running at nv gpu.
+# :code:`DefaultNVGPUTarget`: the model will running at nv gpu.
 target = common.DefaultHostTarget()
 
 ##################################################################
 #
 # Run program and print result
-# ----------------
+# -----------------------------
 # The `build_and_get_output` accepts the input data and finally return the results
 # of model, it has four parameters:
 #
-# :code:`target` the model's irunning target.
+# :code:`target`: the model's irunning target.
 #
-# :code:`tensor_inputs` the model's input variable list.
+# :code:`tensor_inputs`: the model's input variable list.
 #
-# :code:`input_data` the actual data list, the order of the list must be the same as
+# :code:`input_data`: the actual data list, the order of the list must be the same as
 # `tensor_inputs`, otherwise, the resulting error.
 #
-# :code:`tensor_outputs` the model's output variable list, the ordering of the model's
+# :code:`tensor_outputs`: the model's output variable list, the ordering of the model's
 # result list is the same as `tensor_outputs`, here we just has one result `[e]`.
 result = prog.build_and_get_output(target, [a, b, c], tensor_data, [e])
 
