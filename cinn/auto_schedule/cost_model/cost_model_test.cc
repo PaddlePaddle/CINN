ffdcb7d84af0d76d671a6b9bd723dc1a973d38b7
zhhsplendid <zhhsplendid@163.com>
2022-02-14 03:26:12 +0000

    Adding test

--- cinn/auto_schedule/cost_model/cost_model_test.cc
+++ cinn/auto_schedule/cost_model/cost_model_test.cc
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "cinn/auto_schedule/cost_model"
+#include "cinn/auto_schedule/cost_model/cost_model.h"
 
 #include <gtest/gtest.h>
 
