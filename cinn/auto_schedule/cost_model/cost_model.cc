c9973c8699b973cb1766766c0dfd04fdeeb3132d
zhhsplendid <zhhsplendid@163.com>
2022-02-15 08:19:42 +0000

    Quick code for testing, hard code something, have to be reverted

--- cinn/auto_schedule/cost_model/cost_model.cc
+++ cinn/auto_schedule/cost_model/cost_model.cc
@@ -15,18 +15,57 @@
 #include "cinn/auto_schedule/cost_model/cost_model.h"
 
 #include <pybind11/embed.h>
+#include <pybind11/numpy.h>
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 
+#include <cassert>
+#include <cstring>
+#include <iostream>
 #include <string>
 #include <vector>
 
-#include "cinn/pybind/bind_utils.h"
+//#include "cinn/pybind/bind_utils.h"
 
 namespace cinn {
 namespace auto_schedule {
 
-using ::cinn::pybind::VectorToNumpy;
+// using ::cinn::pybind::VectorToNumpy;
+
+// Convert 1D vector to py numpy
+template <typename Dtype>
+pybind11::array VectorToNumpy(const std::vector<Dtype>& vec) {
+  return pybind11::array(pybind11::cast(vec));
+}
+
+// Convert 2D vector to py numpy
+template <typename Dtype>
+pybind11::array VectorToNumpy(const std::vector<std::vector<Dtype>>& vec) {
+  if (vec.size() == 0) {
+    return pybind11::array(pybind11::dtype::of<Dtype>(), {0, 0});
+  }
+
+  std::vector<size_t> shape{vec.size(), vec[0].size()};
+  pybind11::array ret(pybind11::dtype::of<Dtype>(), shape);
+
+  Dtype* py_data = static_cast<Dtype*>(ret.mutable_data());
+  for (size_t i = 0; i < vec.size(); ++i) {
+    assert(vec[i].size() == shape[1] && "Sub vectors must have same size in VectorToNumpy");
+    const Dtype* sub_vec_data = vec[i].data();
+    memcpy(py_data + (shape[0] * i), sub_vec_data, shape[0] * sizeof(Dtype));
+  }
+  return ret;
+}
 
 CostModel::CostModel() {
+  pybind11::module sys_py_mod = pybind11::module::import("sys");
+  // TODO fix the hard code here
+  std::string site_pkg_str = "/usr/local/lib/python3.7/dist-packages";
+  sys_py_mod.attr("path").attr("append")(site_pkg_str);
+  auto path = sys_py_mod.attr("path").cast<std::vector<std::string>>();
+  for (const std::string& s : path) {
+    std::cout << s << std::endl;
+  }
   pybind11::module cost_model_py_mod = pybind11::module::import("cinn.auto_schedule.cost_model");
   python_member_                     = cost_model_py_mod.attr("CostModel")();
 }
@@ -57,9 +96,9 @@ void CostModel::Update(const std::vector<std::vector<float>>& samples, const std
   python_member_.attr("update")(np_samples, np_labels);
 }
 
-void CostModel::Save(const std::string& path) { python_member_.attr("save")(py::str(path)); }
+void CostModel::Save(const std::string& path) { python_member_.attr("save")(pybind11::str(path)); }
 
-void CostModel::Load(const std::string& path) { python_member_.attr("load")(py::str(path)); }
+void CostModel::Load(const std::string& path) { python_member_.attr("load")(pybind11::str(path)); }
 
 }  // namespace auto_schedule
 }  // namespace cinn
