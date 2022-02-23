2a9f653543848d25f0ce8c2af28caf3fd02e1112
Avin0323 <mr.avin0323@gmail.com>
2022-02-23 07:29:19 +0000

    fold transpose into dot with ProgramPass, test=develop

---
+++ cinn/frontend/pass/transpose_folding.cc
@@ -0,0 +1,89 @@
+// Copyright (c) 2021 CINN Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "cinn/frontend/cinn_builder.h"
+#include "cinn/frontend/program_pass.h"
+
+namespace cinn::frontend::pass {
+
+std::vector<Instruction*> TryFoldIntoDot(Instruction* dot,
+                                         const absl::flat_hash_map<std::string, Instruction*>& var_instrs) {
+  std::vector<Instruction*> remove_instrs;
+  if (!(*dot)->attrs.empty()) return remove_instrs;
+  auto is_transpose = [](const Instruction& transpose) {
+    if ("transpose" != transpose->op_type) return false;
+    auto axis = transpose.GetAttrs<std::vector<int>>("axis");
+    for (size_t i = 0; i < axis.size(); ++i) {
+      if (axis[i] + 1 != axis.size() - i) return false;
+    }
+    return true;
+  };
+  for (size_t i = 0; i < (*dot)->inputs.size(); ++i) {
+    auto iter = var_instrs.find((*dot)->inputs[i]->id);
+    if (iter != var_instrs.end()) {
+      const auto& operand = *(iter->second);
+      if (is_transpose(operand)) {
+        // x-> transpose -> out -> dot => x -> dot
+        (*dot)->inputs[i] = operand->inputs[0];
+        (*dot).SetAttr(i == 0 ? "trans_a" : "trans_b", true);
+        remove_instrs.push_back(iter->second);
+      }
+    }
+  }
+  return remove_instrs;
+}
+
+void TransposeFolding(Program* program, const std::unordered_set<std::string>& fetch_ids) {
+  absl::flat_hash_map<std::string, Instruction*> var_instrs;
+  absl::flat_hash_set<Instruction*> remove_instrs;
+  for (size_t i = 0; i < program->size(); ++i) {
+    auto& instr = (*program)[i];
+    for (const auto& out : instr->outputs) {
+      var_instrs[out->id] = &instr;
+    }
+    if ("matmul" == instr->op_type) {
+      for (auto transpose : TryFoldIntoDot(&instr, var_instrs)) {
+        if (fetch_ids.find((*transpose)->outputs[0]->id) == fetch_ids.end()) {
+          remove_instrs.insert(transpose);
+        }
+      }
+    } else {
+      for (const auto& in : instr->inputs) {
+        auto iter = var_instrs.find(in->id);
+        if (iter != var_instrs.end()) {
+          remove_instrs.erase(iter->second);
+        }
+      }
+    }
+  }
+  CinnBuilder builder("transpose_folding_builder");
+  for (auto& var : program->GetInputs()) {
+    builder.CreateInput(var);
+  }
+  for (int i = 0; i < program->size(); i++) {
+    if (remove_instrs.end() != remove_instrs.find(&(*program)[i])) continue;
+    builder.AppendInstruction((*program)[i]);
+  }
+  *program = builder.Build();
+}
+
+}  // namespace cinn::frontend::pass
+
+CINN_REGISTER_HELPER(TransposeFolding) {
+  CINN_REGISTER_PROGRAM_PASS_FUNCTION(TransposeFolding)
+      .describe("This pass folding transpose into dot.")
+      .set_body(cinn::frontend::pass::TransposeFolding);
+
+  return true;
+}
---
+++ cinn/frontend/pass/transpose_folding_test.cc
@@ -0,0 +1,114 @@
+// Copyright (c) 2022 CINN Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <gtest/gtest.h>
+
+#include "cinn/cinn.h"
+#include "cinn/frontend/cinn_builder.h"
+#include "cinn/frontend/pass/use_program_pass.h"
+#include "cinn/frontend/program_pass.h"
+#include "cinn/frontend/syntax.h"
+#include "cinn/hlir/framework/graph.h"
+#include "cinn/hlir/framework/graph_compiler.h"
+#include "cinn/hlir/framework/pass.h"
+#include "cinn/hlir/op/use_ops.h"
+#include "cinn/hlir/pass/use_pass.h"
+
+namespace cinn::frontend {
+
+Target GetTarget() {
+#ifdef CINN_WITH_CUDA
+  return common::DefaultNVGPUTarget();
+#else
+  return common::DefaultHostTarget();
+#endif
+}
+
+void SetRandData(const hlir::framework::Tensor& tensor, Target target) {
+#ifdef CINN_WITH_CUDA
+  auto* data = tensor->mutable_data<float>(target);
+  std::vector<float> host_memory(tensor->shape().numel(), 0);
+  for (float& v : host_memory) {
+    v = (rand() * 1.f) / RAND_MAX;  // All random data
+  }
+  CUDA_CALL(cudaMemcpy(reinterpret_cast<void*>(data),
+                       host_memory.data(),
+                       tensor->shape().numel() * sizeof(float),
+                       cudaMemcpyHostToDevice));
+#else
+  auto* data = tensor->mutable_data<float>(target);
+  for (size_t j = 0; j < tensor->shape().numel(); j++) {
+    data[j] = (rand() * 1.f) / RAND_MAX;  // All random data
+  }
+#endif
+}
+
+std::vector<float> GetTensorData(const hlir::framework::Tensor& tensor, Target target) {
+  std::vector<float> data;
+#ifdef CINN_WITH_CUDA
+  data.resize(tensor->shape().numel());
+  CUDA_CALL(cudaMemcpy(data.data(),
+                       reinterpret_cast<void*>(tensor->mutable_data<float>(target)),
+                       tensor->shape().numel() * sizeof(float),
+                       cudaMemcpyDeviceToHost));
+#else
+  for (size_t i = 0; i < tensor->shape().numel(); ++i) {
+    data.push_back(tensor->data<float>()[i]);
+  }
+#endif
+  return data;
+}
+
+void RunWithProgram(const Program& program,
+                    const Target& target,
+                    const std::shared_ptr<hlir::framework::Scope>& scope) {
+  auto graph = std::make_shared<hlir::framework::Graph>(program, target);
+  hlir::framework::ApplyPasses(graph.get(), {"InferShape", "OpFusion"});
+  VLOG(1) << "graph:\n" << graph->Visualize();
+  hlir::framework::GraphCompiler gc(target, scope, graph);
+  auto runtime_program = gc.Build();
+  runtime_program->Execute();
+}
+
+TEST(TransposeFolding, FoldIntoDot) {
+  CinnBuilder builder("cinn_builder");
+  auto x           = builder.CreateInput(Float(32), {2, 3}, "X");
+  auto y           = builder.CreateInput(Float(32), {2, 3}, "Y");
+  auto transpose_y = builder.Transpose(y, {1, 0});
+  auto out         = builder.Dot(x, transpose_y);
+  auto program     = builder.Build();
+  auto target      = GetTarget();
+  auto graph       = std::make_shared<hlir::framework::Graph>(program, target);
+  auto scope       = hlir::framework::BuildScope(target, graph);
+  scope->Var<hlir::framework::Tensor>("X");
+  scope->Var<hlir::framework::Tensor>("Y");
+  SetRandData(scope->GetTensor("X"), target);
+  SetRandData(scope->GetTensor("Y"), target);
+  size_t origin_size = program.size();
+  VLOG(1) << "Program:\n" << program;
+  RunWithProgram(program, target, scope);
+  auto origin_out = GetTensorData(scope->GetTensor(out->id), target);
+  ApplyPass(&program, {}, "TransposeFolding");
+  size_t folded_size = program.size();
+  VLOG(1) << "Program:\n" << program;
+  RunWithProgram(program, target, scope);
+  auto folded_out = GetTensorData(scope->GetTensor(out->id), target);
+  ASSERT_EQ(origin_size, folded_size + 1);
+  ASSERT_EQ(origin_out.size(), folded_out.size());
+  for (size_t i = 0; i < origin_out.size(); ++i) {
+    ASSERT_FLOAT_EQ(origin_out[i], folded_out[i]);
+  }
+}
+
+}  // namespace cinn::frontend
