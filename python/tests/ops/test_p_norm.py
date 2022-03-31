77a976fa601ace6162a8c46f51bf79c81396529c
jiangcheng <thisjiang@qq.com>
2022-03-31 09:14:22 +0000

    add op norm's single test script

---
+++ python/tests/ops/test_p_norm.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2021 CINN Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+import numpy as np
+from op_test import OpTest, OpTestTool
+import paddle
+import paddle.nn.functional as F
+import cinn
+from cinn.frontend import *
+from cinn.common import *
+import sys
+
+
+@OpTestTool.skip_if(not is_compiled_with_cuda(),
+                    "x86 test will be skipped due to timeout.")
+class TestPNormOp(OpTest):
+    def setUp(self):
+        self.init_case()
+
+    def init_case(self):
+        self.inputs = {"x": np.random.random((10201)).astype("float32")}
+
+    def build_paddle_program(self, target):
+        x = paddle.to_tensor(self.inputs["x"], stop_gradient=False)
+        out = paddle.norm(x, p=2)
+        self.paddle_outputs = [out]
+
+    def build_cinn_program(self, target):
+        builder = NetBuilder("p_norm")
+
+        x = builder.create_input(Float(32), self.inputs["x"].shape, "x")
+
+        out_mul = builder.elementwise_mul(x, x)
+        out_reduce = builder.reduce_sum(out_mul, dim=[0], keep_dim=False)
+        out = builder.sqrt(out_reduce)
+
+        prog = builder.build()
+        forward_res = self.get_cinn_output(prog, target, [x],
+                                           [self.inputs["x"]], [out])
+
+        self.cinn_outputs = forward_res
+
+    def test_check_results(self):
+        self.check_outputs_and_grads()
+
+
+if __name__ == "__main__":
+    unittest.main()
