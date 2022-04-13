2a56d792e3bd2f929c9e63857a83ac2f06bad607
sunli <466530738@qq.com>
2022-04-13 14:22:25 +0000

    add test

--- cinn/hlir/framework/op_lowering_test.cc
+++ cinn/hlir/framework/op_lowering_test.cc
@@ -465,6 +465,44 @@ TEST(OP_LOWERING, Reduce_Test_8) {
   }
 }
 
+TEST(OP_LOWERING, Reduce_Test_9) {
+  int c = 128, h = 128, w = 128;
+  NetBuilder net_builder("Reduce_Test_9");
+  // create model
+  {
+    auto A = net_builder.CreateInput(Float(32), {c, h, w}, "A");
+    auto B = net_builder.CreateInput(Float(32), {c, h, w}, "B");
+    auto C = net_builder.CreateInput(Float(32), {h}, "C");
+    auto D = net_builder.ElementwiseAdd(A, B);
+    auto E = net_builder.Reduce(D, ReduceKind::kSum, {0, 2});
+    auto F = net_builder.Reduce(D, ReduceKind::kSum, {0, 2});
+    auto G = net_builder.ElementwiseAdd(E, C);
+    auto I = net_builder.ElementwiseAdd(F, C);
+  }
+
+  auto program = net_builder.Build();
+  auto target  = GetTarget();
+  RunDecomposer(&program, target);
+
+  auto graph = std::make_shared<hlir::framework::Graph>(program, target);
+  hlir::framework::ApplyPass(graph.get(), "OpFusionPass");
+  CHECK_EQ(graph->fusion_groups.size(), 5);
+
+  hlir::framework::ApplyPass(graph.get(), "FusionMergePass");
+  CHECK_EQ(graph->fusion_groups.size(), 1);
+
+  auto& dtype_dict = graph->GetMutableAttrs<absl::flat_hash_map<std::string, Type>>("inferdtype");
+  auto& shape_dict = graph->GetMutableAttrs<absl::flat_hash_map<std::string, shape_t>>("infershape");
+
+  OpLowerer op_lowerer(dtype_dict, shape_dict, target);
+  for (auto& fusion_op : graph->fusion_groups) {
+    auto lowered_func = op_lowerer.Lower(fusion_op);
+    CHECK_EQ(lowered_func.size(), 1);
+    LOG(INFO) << lowered_func[0];
+    CodeGen(lowered_func[0]);
+  }
+}
+
 }  // namespace framework
 }  // namespace hlir
 }  // namespace cinn
