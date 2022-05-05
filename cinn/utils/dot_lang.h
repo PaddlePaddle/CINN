00b959b7c180ed66feabcdd0173332189f5b1aca
Yiqun Liu <liuyiqun01@baidu.com>
2022-05-05 15:28:29 +0800

    Visualize each group in graph. (#756)

--- cinn/utils/dot_lang.h
+++ cinn/utils/dot_lang.h
@@ -13,21 +13,22 @@
 // limitations under the License.
 
 #pragma once
-#include <glog/logging.h>
 
 #include <map>
-#include <sstream>
+#include <set>
 #include <string>
 #include <vector>
 
 namespace cinn {
 namespace utils {
 
-static size_t dot_node_counter{0};
+void ResetDotCounters();
 
-struct Node;
-struct Edge;
-struct Attr;
+struct DotNode;
+struct DotCluster;
+struct DotEdge;
+struct DotAttr;
+
 /*
  * A Dot template that helps to build a DOT graph definition.
  */
@@ -35,15 +36,26 @@ class DotLang {
  public:
   DotLang() = default;
 
-  explicit DotLang(const std::vector<Attr>& attrs) : attrs_(attrs) {}
+  explicit DotLang(const std::vector<DotAttr>& attrs) : attrs_(attrs) {}
+
   /**
-   * Add a node ot DOT graph.
+   * Add a node to the DOT graph.
    * @param id Unique ID for this node.
    * @param attrs DOT attributes.
    * @param label Name of the node.
    */
+  void AddNode(const std::string& id,
+               const std::vector<DotAttr>& attrs,
+               std::string label      = "",
+               std::string cluster_id = "",
+               bool allow_duplicate   = false);
 
-  void AddNode(const std::string& id, const std::vector<Attr>& attrs, std::string label = "");
+  /**
+   * Add a subgraph to the DOT graph.
+   * @param id Unique ID for this subgraph.
+   * @param attrs DOT attributes.
+   */
+  void AddCluster(const std::string& id, const std::vector<DotAttr>& attrs);
 
   /**
    * Add an edge to the DOT graph.
@@ -51,7 +63,7 @@ class DotLang {
    * @param target The id of the sink of the edge.
    * @param attrs The attributes of the edge.
    */
-  void AddEdge(const std::string& source, const std::string& target, const std::vector<Attr>& attrs);
+  void AddEdge(const std::string& source, const std::string& target, const std::vector<DotAttr>& attrs);
 
   std::string operator()() const { return Build(); }
 
@@ -59,40 +71,61 @@ class DotLang {
   // Compile to DOT language codes.
   std::string Build() const;
 
-  std::map<std::string, Node> nodes_;
-  std::vector<Edge> edges_;
-  std::vector<Attr> attrs_;
+  std::map<std::string, DotNode> nodes_;
+  std::map<std::string, DotCluster> clusters_;
+  std::vector<DotEdge> edges_;
+  std::vector<DotAttr> attrs_;
 };
 
-struct Attr {
+struct DotAttr {
   std::string key;
   std::string value;
 
-  Attr(const std::string& key, const std::string& value) : key(key), value(value) {}
+  DotAttr(const std::string& key, const std::string& value) : key(key), value(value) {}
 
   std::string repr() const;
 };
 
-struct Node {
+struct DotNode {
   std::string name;
-  std::vector<Attr> attrs;
+  std::vector<DotAttr> attrs;
 
-  Node(const std::string& name, const std::vector<Attr>& attrs);
+  DotNode() = default;
+  DotNode(const std::string& name, const std::vector<DotAttr>& attrs, const std::string& cluster_id);
 
   std::string id() const { return id_; }
+  std::string cluster_id() const { return cluster_id_; }
 
   std::string repr() const;
 
  private:
   std::string id_;
+  std::string cluster_id_;
 };
 
-struct Edge {
+struct DotCluster {
+  std::string name;
+  std::vector<DotAttr> attrs;
+
+  DotCluster() = default;
+  DotCluster(const std::string& name, const std::vector<DotAttr>& attrs);
+
+  void Insert(DotNode* node) { nodes_.insert(node); }
+
+  std::string id() const { return id_; }
+  std::set<DotNode*> nodes() const { return nodes_; }
+
+ private:
+  std::string id_;
+  std::set<DotNode*> nodes_;  // Not owned
+};
+
+struct DotEdge {
   std::string source;
   std::string target;
-  std::vector<Attr> attrs;
+  std::vector<DotAttr> attrs;
 
-  Edge(const std::string& source, const std::string& target, const std::vector<Attr>& attrs)
+  DotEdge(const std::string& source, const std::string& target, const std::vector<DotAttr>& attrs)
       : source(source), target(target), attrs(attrs) {}
 
   std::string repr() const;
