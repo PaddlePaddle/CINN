2c970ca3c95a33fb738e39445d2b74b350680abe
TeFeng Chen <ctfeng66@163.com>
2022-05-14 16:14:40 +0800

    element-wise op use gpu vectorize (#786)
    * element-wise op use gpu vectorize
    
    * add annotations and simplify code
    
    * correct the vectorize level

--- cinn/runtime/flags.cc
+++ cinn/runtime/flags.cc
@@ -40,6 +40,9 @@ DEFINE_bool(cinn_sync_run,
 DEFINE_string(cinn_fusion_groups_graphviz_dir,
               StringFromEnv("FLAGS_cinn_fusion_groups_graphviz_dir", ""),
               "Specify the directory path of dot file of graph, which is used for debug.");
+DEFINE_bool(cinn_use_cuda_vectorize,
+            BoolFromEnv("FLAGS_cinn_use_cuda_vectorize", false),
+            "Whether use cuda vectroize on schedule config");
 
 namespace cinn {
 namespace runtime {
