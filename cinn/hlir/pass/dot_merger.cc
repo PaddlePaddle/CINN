1a99261f7d3b7a50bd3aa80a6da0c676cbc99592
Shixiaowei02 <39303645+Shixiaowei02@users.noreply.github.com>
2022-06-17 09:16:22 +0000

    polish codes

--- cinn/hlir/pass/dot_merger.cc
+++ cinn/hlir/pass/dot_merger.cc
@@ -55,10 +55,8 @@ bool accessible(GraphNode* start, GraphNode* end) {
 template <typename T>
 T get_attr(Node* instr, const std::string& attr, T def) {
   if (!instr->attrs.attr_store.count(attr)) {
-    LOG(INFO) << "not find attr: " << attr;
     return def;
   }
-  LOG(INFO) << "found attr: " << attr << " = " << absl::get<T>(instr->attrs.attr_store.at(attr));
   return absl::get<T>(instr->attrs.attr_store.at(attr));
 }
 
@@ -154,7 +152,6 @@ class DotBuilder {
   const shape_dict_t& shape_dict() const { return shape_dict_; };
 
   NodeData* Var(common::Shared<Node>& producer) {
-    LOG(INFO) << "DotBuilder::Var";
     auto* res = new NodeData(producer, 0, 0, "var___dot_merger_" + std::to_string(idx_++), false);
     graph_->RegisterNode(producer->id(), res);
     graph_->RegisterNode(res->id(), producer.get());
@@ -164,14 +161,10 @@ class DotBuilder {
   }
 
   NodeData* Concat(int axis, std::vector<NodeData*> inputs) {
-    LOG(INFO) << "DotBuilder::Concat";
     const std::string type{"concat"};
-    LOG(INFO) << "DotBuilder::Concat";
     Node* tmp = new Node(
         framework::Operator::Get(type), type + "__dot_merger_", type + "__dot_merger_" + std::to_string(idx_++));
-    LOG(INFO) << "DotBuilder::Concat";
     common::Shared<Node> instr(tmp);
-    LOG(INFO) << "DotBuilder::Concat";
     // auto instr                      = std::make_shared<Node>(framework::Operator::Get(type), gen_name(type));
     instr->attrs.attr_store["axis"] = axis;
     for (auto* in : inputs) {
@@ -182,7 +175,6 @@ class DotBuilder {
   }
 
   NodeData* Matmul(bool trans_a, bool trans_b, bool trans_out, float alpha, NodeData* lhs, NodeData* rhs) {
-    LOG(INFO) << "DotBuilder::Matmul";
     const std::string type{"matmul"};
     auto instr                           = common::Shared<Node>(new Node(
         framework::Operator::Get(type), type + "__dot_merger_", type + "__dot_merger_" + std::to_string(idx_++)));
@@ -199,7 +191,6 @@ class DotBuilder {
 
   NodeData* Slice(
       std::vector<int> axes, std::vector<int> starts, std::vector<int> ends, NodeData* input, NodeData* output) {
-    LOG(INFO) << "DotBuilder::Slice";
     const std::string type{"slice"};
     common::Shared<Node> instr(new Node(
         framework::Operator::Get(type), type + "__dot_merger_", type + "__dot_merger_" + std::to_string(idx_++)));
@@ -239,38 +230,28 @@ class DotMergerPass {
     std::set<Node*> nodes_to_remove;
     DotBuilder builder(graph);
     for (auto& c : clusters) {
-      LOG(INFO) << "cluster: " << c.first->id() << ", size = " << c.second.size();
       auto& dots = c.second;
       for (size_t i = 0; i < dots.size(); ++i) {
         auto*& a = dots[i];
         if (!a) {
-          LOG(INFO) << "a is null!";
           continue;
         }
         for (size_t j = i + 1; j < dots.size(); ++j) {
           auto* b = dots[j];
           if (!b) {
-            LOG(INFO) << "b is null!";
             continue;
           }
-          LOG(INFO) << "i =" << i << ", j = " << j << "(" << a->id() << "," << b->id() << ")";
           if (nodes_to_remove.count(a) || nodes_to_remove.count(b)) {
-            LOG(INFO) << "remove!!";
             continue;
           }
           if (accessible(a, b)) {
-            LOG(INFO) << "1 access : " << a->id() << ", " << b->id();
             continue;
           }
           if (accessible(b, a)) {
-            LOG(INFO) << "2 access : " << a->id() << ", " << b->id();
             continue;
           }
-          LOG(INFO) << "fuse: " << a->id() << " (" << a << "), " << b->id() << " (" << b << "), ";
           auto* merged = MergeDots(&builder, a, b);
           if (merged) {
-            LOG(INFO) << "--- nodes_to_remove: " << a << ", " << b;
-            LOG(INFO) << "--- nodes_to_remove: " << a->id() << " (" << a << "), " << b->id() << " (" << b << "), ";
             nodes_to_remove.insert(a);
             nodes_to_remove.insert(b);
             dots[i] = merged;
@@ -314,16 +295,15 @@ class DotMergerPass {
     CHECK(a && b) << "The pointer of node is illegal.";
     const std::array<bool, 2> trans_a{get_attr<bool>(a, "trans_a", false), get_attr<bool>(b, "trans_a", false)};
     const std::array<bool, 2> trans_b{get_attr<bool>(a, "trans_b", false), get_attr<bool>(b, "trans_b", false)};
-    // const std::array<bool, 2> trans_out{get_attr<bool>(a, "trans_out"), get_attr<bool>(b, "trans_out")};
+    const std::array<bool, 2> trans_out{get_attr<bool>(a, "trans_out", false), get_attr<bool>(b, "trans_out", false)};
     const std::array<float, 2> alpha{get_attr<float>(a, "alpha", 1.f), get_attr<float>(b, "alpha", 1.f)};
-    if (!all_equal(trans_a, trans_b, alpha)) {
+    if (!all_equal(trans_a, trans_b, trans_out, alpha)) {
       return nullptr;
     }
     bool lhs{true};
     int axis{1};
     NodeData *shared_input{}, *input_a{}, *input_b{};
     if (input_operand(a, 1) == input_operand(b, 1)) {
-      LOG(INFO) << "input_operand(a, 1) == input_operand(b, 1)";
       shared_input = input_operand(a, 1);
       input_a      = input_operand(a, 0);
       input_b      = input_operand(b, 0);
@@ -334,36 +314,28 @@ class DotMergerPass {
         axis = 0;
       }
     } else if (input_operand(a, 0) == input_operand(b, 0)) {
-      LOG(INFO) << "input_operand(a, 0) == input_operand(b, 0)";
       shared_input = input_operand(a, 0);
       input_a      = input_operand(a, 1);
       input_b      = input_operand(b, 1);
     } else {
-      LOG(INFO) << "not fuse!!";
       return nullptr;
     }
     auto* output_a   = output_operand(a, 0);
     auto* output_b   = output_operand(b, 0);
     auto& graph_outs = builder->graph()->outputs;
-    LOG(INFO) << "graph_outs.size = " << graph_outs.size();
     for (auto* out : graph_outs) {
-      LOG(INFO) << "graph outputs: " << out->id();
     }
     for (auto* n : {shared_input, input_a, input_b, output_a, output_b}) {
       if (std::find(graph_outs.begin(), graph_outs.end(), n) != graph_outs.end()) {
-        LOG(INFO) << " -- EXIT: graph_out : " << n->id();
         return nullptr;
       }
     }
-    LOG(INFO) << shared_input->id() << ", " << input_a->id() << ", " << input_b->id() << ", axis=" << axis;
     CHECK(shared_input && input_a && input_b) << "The input node type must be variable.";
     // CHECK(builder->shape_dict().count(input_a->id())) << "not found " << input_a->id();
     auto shape_shared = builder->shape_dict().at(shared_input->id());
     auto shape_a      = builder->shape_dict().at(input_a->id());
     // CHECK(builder->shape_dict().count(input_b->id())) << "not found " << input_b->id();
     auto shape_b = builder->shape_dict().at(input_b->id());
-    LOG(INFO) << "shape_shared: " << shape_shared[0] << "," << shape_shared[1];
-    LOG(INFO) << "shape_a : " << shape_a[0] << ", " << shape_a[1] << ";" << shape_b[0] << ", " << shape_b[1];
     CHECK_EQ(shape_a[1 - axis], shape_b[1 - axis])
         << "The shape of matmul is error. " << shape_a.size() << ", " << shape_b.size();
     auto* concat_out = builder->Concat(axis, {input_a, input_b});
@@ -382,28 +354,8 @@ class DotMergerPass {
 }  // namespace
 
 void DotMergerPassFunc(framework::Graph* graph) {
-  {
-    std::string str = graph->Visualize();
-    std::stringstream ss;
-    ss << str;
-    std::string myString = ss.str();
-
-    std::ofstream file("before.txt", std::ofstream::out | std::ofstream::trunc);
-    file << myString;
-  }
-
   DotMergerPass pass;
   pass.Apply(graph);
-
-  {
-    std::string str = graph->Visualize();
-    std::stringstream ss;
-    ss << str;
-    std::string myString = ss.str();
-
-    std::ofstream file("after.txt", std::ofstream::out | std::ofstream::trunc);
-    file << myString;
-  }
 }
 
 }  // namespace pass
