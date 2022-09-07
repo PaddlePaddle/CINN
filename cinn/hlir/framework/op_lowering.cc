83948352338dee7f7520832279d8ecc8d737d810
sunli <sunli11@baidu.com>
2022-09-07 18:42:14 +0800

    add custom call op (#920)
    * add custom call op

--- cinn/hlir/framework/op_lowering.cc
+++ cinn/hlir/framework/op_lowering.cc
@@ -1110,8 +1110,15 @@ std::vector<ir::LoweredFunc> OpLowerer::IRLowerOpaqueOp(GroupPtr& group) {
   }
 
   auto impl = OpStrategy::SelectImpl(cinn_strategy[node->op()](node->attrs, inputs, out_types, out_shapes, target_));
-  common::CINNValuePack pack = impl->fcompute(common::CINNValuePack{cinn_inputs});
+  // if node op is custom call, return compute.
+  if (node->op()->name == "custom_call") {
+    cinn_inputs.push_back(common::CINNValue(group->GetFuncName()));
+    common::CINNValuePack pack = impl->fcompute(common::CINNValuePack{cinn_inputs});
+    CHECK_EQ(pack.size(), 1UL);
+    return {pack[0].operator ir::Expr().as_lowered_func_ref()};
+  }
 
+  common::CINNValuePack pack = impl->fcompute(common::CINNValuePack{cinn_inputs});
   for (int i = 0; i < pack->size() - 1; i++) {
     ir::Expr temp = pack[i];
     // checkout whether the tensor is with buffer.
