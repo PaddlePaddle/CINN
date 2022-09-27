a432b86befcf6f0cca9f390a0b8b731906d55822
jiangcheng <thisjiang@qq.com>
2022-09-27 17:30:43 +0800

    using cinn::utils::Flatten not self-defined flattenvector

--- cinn/frontend/net_builder.h
+++ cinn/frontend/net_builder.h
@@ -103,7 +103,7 @@ namespace frontend {
 
 // ******************************************* //
 // Reduce array elements over the given dims.
-// Variable REDUCE_OP(const Variable& x, const std::vector<int>& dim = {}, bool keep_dim = false);
+// Variable REDUCE_OP(const Variable& x, const cinn::utils::ShapeType& dim = {}, bool keep_dim = false);
 #define NETBUILDER_REDUCE_OP_FOREACH(macro__) \
   macro__(ReduceSum) \
   macro__(ReduceProd) \
@@ -180,53 +180,31 @@ class NetBuilder {
    */
   Variable Reduce(const std::string& op_type,
                   const Variable& x,
-                  const std::vector<int>& dim = {},
-                  bool keep_dim               = false);
+                  const cinn::utils::ShapeType& dim = {},
+                  bool keep_dim                     = false);
 
  private:
   // the helper function for Matmul API
   std::pair<Variable, Variable> BroadcastMatmulInput(
       const Variable& x, const Variable& y, bool trans_x, bool trans_y, float alpha);
-  std::vector<int> GetMatmulOutputShape(const Variable& x, const Variable& y, bool trans_x, bool trans_y, float alpha);
+  cinn::utils::ShapeType GetMatmulOutputShape(
+      const Variable& x, const Variable& y, bool trans_x, bool trans_y, float alpha);
 
   // the helper function for Constant API
   template <typename T>
-  std::enable_if_t<std::is_arithmetic<T>::value, void> FlattenVector(const std::vector<T>& value,
-                                                                     std::vector<cinn::utils::Attribute>* all_datas,
-                                                                     std::vector<int>* shape,
-                                                                     int depth) {
-    if (shape->size() == depth) {
-      shape->emplace_back(value.size());
-    } else {
-      CHECK_GT(shape->size(), depth) << "The Shape size should greater than recursive depth.";
-      CHECK_EQ(value.size(), shape->at(depth))
-          << "The sub_vector.size() for any sub_vector=vector[0][...][n] should be the same, but vector[0][..]["
-          << depth << "] not! Please check.";
-    }
-
-    all_datas->reserve(all_datas->size() + value.size());
-    for (auto ele : value) {
-      all_datas->emplace_back(ele);
-    }
+  std::enable_if_t<std::is_arithmetic<T>::value, cinn::utils::ShapeType> GetVectorShape(const std::vector<T>& value) {
+    CHECK(!value.empty()) << "The vector should not has empty list! Please check.";
+    return {static_cast<int>(value.size())};
   }
 
   template <typename T>
-  std::enable_if_t<cinn::utils::is_vector<T>::value, void> FlattenVector(const std::vector<T>& value,
-                                                                         std::vector<cinn::utils::Attribute>* all_datas,
-                                                                         std::vector<int>* shape,
-                                                                         int depth) {
-    if (shape->size() == depth) {
-      shape->emplace_back(value.size());
-    } else {
-      CHECK_GT(shape->size(), depth) << "The Shape size should greater than recursive depth.";
-      CHECK_EQ(value.size(), shape->at(depth))
-          << "The sub_vector.size() for any sub_vector=vector[0][...][n] should be the same, but vector[0][..]["
-          << depth << "] not! Please check.";
-    }
+  std::enable_if_t<cinn::utils::is_vector<T>::value, cinn::utils::ShapeType> GetVectorShape(
+      const std::vector<T>& value) {
+    CHECK(!value.empty()) << "The vector should not has empty list! Please check.";
 
-    for (const auto& vec : value) {
-      FlattenVector(vec, all_datas, shape, depth + 1);
-    }
+    auto shape = GetVectorShape(value[0]);
+    shape.insert(shape.begin(), static_cast<int>(value.size()));
+    return shape;
   }
 
  public:
@@ -306,24 +284,24 @@ class NetBuilder {
 
   Variable Gather(const Variable& x, const Variable& index, const int& axis = 0);
 
-  Variable GatherNd(const Variable& x, const Variable& index, const std::vector<int>& axes = {});
+  Variable GatherNd(const Variable& x, const Variable& index, const cinn::utils::ShapeType& axes = {});
 
   Variable Scatter(const Variable& src, const Variable& index, const Variable& out, const int& axis = 0);
   Variable Scatter(const Variable& src,
                    const Variable& index,
-                   const std::vector<int>& shape,
+                   const cinn::utils::ShapeType& shape,
                    const float& default_value = 0,
                    const int& axis            = 0);
 
   Variable ScatterNd(const Variable& src,
                      const Variable& index,
                      const Variable& out,
-                     const std::vector<int>& axes = {});
+                     const cinn::utils::ShapeType& axes = {});
   Variable ScatterNd(const Variable& src,
                      const Variable& index,
-                     const std::vector<int>& shape,
-                     const float& default_value   = 0,
-                     const std::vector<int>& axes = {});
+                     const cinn::utils::ShapeType& shape,
+                     const float& default_value         = 0,
+                     const cinn::utils::ShapeType& axes = {});
 
   /**
    * @brief This operator checks if all `x` and `y` satisfy the condition: `|x - y| <= atol + rtol * |y|`
@@ -349,7 +327,7 @@ class NetBuilder {
    * @return The result variable.
    */
 #define NETBUILDER_REDUCE_OP_DECL(func_name__) \
-  Variable func_name__(const Variable& x, const std::vector<int>& dim = {}, bool keep_dim = false);
+  Variable func_name__(const Variable& x, const cinn::utils::ShapeType& dim = {}, bool keep_dim = false);
   NETBUILDER_REDUCE_OP_FOREACH(NETBUILDER_REDUCE_OP_DECL)
 #undef NETBUILDER_REDUCE_OP_DECL
 
@@ -369,7 +347,9 @@ class NetBuilder {
    * @param id_hint The input variable's name. Default is None.
    * @return The new input.
    */
-  Placeholder CreateInput(const common::Type& type, const std::vector<int>& shape, const std::string& id_hint = "");
+  Placeholder CreateInput(const common::Type& type,
+                          const cinn::utils::ShapeType& shape,
+                          const std::string& id_hint = "");
 
   /**
    * @brief Create constant tensor with the specific value/vector and type, the type is infered from value.
@@ -396,41 +376,19 @@ class NetBuilder {
   std::enable_if_t<cinn::utils::is_vector<T>::value, Variable> Constant(const T& value, const std::string& name) {
     CHECK(!value.empty()) << "The value of Constant should not be None or empty list! Please check.";
 
-    // flatten n-dims vector to 1-dim vector, and get the real shape
-    std::vector<int> shape;
-    std::vector<cinn::utils::Attribute> all_datas;
-    FlattenVector(value, &all_datas, &shape, 0);
+    // flatten n-dims vector to 1-dim vector
+    auto all_datas = cinn::utils::Flatten(value);
 
-    VLOG(4) << "The input of constant has " << all_datas.size() << " elements, it's output is " << name << "["
-            << cinn::utils::Join(shape, ", ") << "]";
+    // why need static_cast ? For vector<bool>, the type of `data[i]` is `bit_reference` no `bool`
+    using dtype = typename decltype(all_datas)::value_type;
 
     std::vector<Variable> vars;
     vars.reserve(all_datas.size());
-
     for (int i = 0; i < all_datas.size(); ++i) {
-      std::string var_name = name + "_" + std::to_string(i);
-      const auto& cur_data = all_datas[i];
-
-      // get the real dtype from Attribute
-      Variable var;
-      if (absl::get_if<bool>(&cur_data)) {
-        VLOG(4) << "The input dtype of constant is bool";
-        var = Constant(absl::get<bool>(cur_data), var_name);
-      } else if (absl::get_if<int>(&cur_data)) {
-        VLOG(4) << "The input dtype of constant is int32";
-        var = Constant(absl::get<int>(cur_data), var_name);
-      } else if (absl::get_if<float>(&cur_data)) {
-        VLOG(4) << "The input dtype of constant is float32";
-        var = Constant(absl::get<float>(cur_data), var_name);
-      } else {
-        // TODO(thisjiang): support other dtype when Attribute supported, and modify pybind dtype also
-        LOG(FATAL) << "Constant only support bool/int/float! Please check.";
-      }
-
-      vars.emplace_back(var);
+      vars.emplace_back(Constant(static_cast<dtype>(all_datas[i]), name + "_" + std::to_string(i)));
     }
 
-    auto out = Reshape(Concat(vars), shape);
+    auto out = Reshape(Concat(vars), GetVectorShape(value));
 
     // set the name correctly
     out.set_id(name);
@@ -446,7 +404,7 @@ class NetBuilder {
    * @param force_cpu Whether the variable should force placed in cpu, default in device memory. Default is false.
    * @return The result variable.
    */
-  Variable FillConstant(const std::vector<int>& shape,
+  Variable FillConstant(const cinn::utils::ShapeType& shape,
                         float value,
                         const std::string& name,
                         const std::string& dtype,
@@ -461,7 +419,7 @@ class NetBuilder {
    * @return The result variable.
    */
   template <typename T = float>
-  Variable FillConstant(const std::vector<int>& shape, T value, const std::string& name, bool force_cpu = false) {
+  Variable FillConstant(const cinn::utils::ShapeType& shape, T value, const std::string& name, bool force_cpu = false) {
     return FillConstant(shape, static_cast<float>(value), name, common::Type2Str(common::type_of<T>()), force_cpu);
   }
 
@@ -552,7 +510,7 @@ class NetBuilder {
    * dimension unchanged.
    * @return The result variable with given shape.
    */
-  Variable BroadcastTo(const Variable& x, const std::vector<int>& out_shape);
+  Variable BroadcastTo(const Variable& x, const cinn::utils::ShapeType& out_shape);
 
   /**
    * @brief Broadcast the input variable to a given shape.
@@ -563,7 +521,9 @@ class NetBuilder {
    * the same as input shape.
    * @return The result variable with given shape.
    */
-  Variable BroadcastTo(const Variable& x, const std::vector<int>& out_shape, const std::vector<int>& broadcast_axes);
+  Variable BroadcastTo(const Variable& x,
+                       const cinn::utils::ShapeType& out_shape,
+                       const cinn::utils::ShapeType& broadcast_axes);
 
   // *******************************************
   // Data Layout transform operator
@@ -595,7 +555,7 @@ class NetBuilder {
    * @param shape Define the target shape. At most one dimension of the target shape can be -1.
    * @return A reshaped variable with the same data type as x.
    */
-  Variable Reshape(const Variable& x, const std::vector<int>& shape);
+  Variable Reshape(const Variable& x, const cinn::utils::ShapeType& shape);
 
   /**
    * @brief This OP will squeeze single-dimensional entries of input variable shape. If axes is provided, will remove
@@ -606,7 +566,7 @@ class NetBuilder {
    * `axes=axes+rank(input)`.
    * @return Output squeezed variable. Data type is same as input variable.
    */
-  Variable Squeeze(const Variable& x, const std::vector<int>& axes);
+  Variable Squeeze(const Variable& x, const cinn::utils::ShapeType& axes);
 
   /**
    * @brief This operator reverse the input along the axis.
@@ -614,7 +574,7 @@ class NetBuilder {
    * @param axis Specify the axis to operate on the input reverse.
    * @return A reversed variable with the same data type as x.
    */
-  Variable Reverse(const Variable& x, const std::vector<int>& axis);
+  Variable Reverse(const Variable& x, const cinn::utils::ShapeType& axis);
 
   /**
    * @brief Permute the data dimensions of input according to perm. The i-th dimension of the returned variable will
@@ -623,7 +583,7 @@ class NetBuilder {
    * @param axis Permute the input according to the data of perm.
    * @return A transposed n-D variable.
    */
-  Variable Transpose(const Variable& x, const std::vector<int>& axis);
+  Variable Transpose(const Variable& x, const cinn::utils::ShapeType& axis);
 
   /**
    * @brief This operator produces a slice of x along multiple axes.
@@ -636,7 +596,7 @@ class NetBuilder {
    * @return A variable with the same dimension as x. The data type is same as x.
    */
   Variable Slice(const Variable& x,
-                 const std::vector<int>& axes,
+                 const cinn::utils::ShapeType& axes,
                  const std::vector<int>& starts      = {},
                  const std::vector<int>& ends        = {},
                  const std::vector<int>& infer_flags = {},
@@ -691,7 +651,7 @@ class NetBuilder {
    */
   Variable SliceAssign(const Variable& x,
                        const Variable& assign,
-                       const std::vector<int>& axes,
+                       const cinn::utils::ShapeType& axes,
                        const std::vector<int>& starts,
                        const std::vector<int>& ends,
                        const std::vector<int>& strides = {});
@@ -771,14 +731,14 @@ class NetBuilder {
    */
   Variable Conv(const Variable& x,
                 const Variable& weight,
-                const std::vector<int>& strides      = {1, 1},
-                const std::vector<int>& paddings     = {0, 0},
-                const std::vector<int>& dilations    = {1, 1},
-                int groups                           = 1,
-                const std::string& conv_type         = "forward",
-                const std::string& data_format       = "NCHW",
-                const std::string& padding_algorithm = "EXPLICIT",
-                const std::vector<int>& output_shape = {});
+                const std::vector<int>& strides            = {1, 1},
+                const std::vector<int>& paddings           = {0, 0},
+                const std::vector<int>& dilations          = {1, 1},
+                int groups                                 = 1,
+                const std::string& conv_type               = "forward",
+                const std::string& data_format             = "NCHW",
+                const std::string& padding_algorithm       = "EXPLICIT",
+                const cinn::utils::ShapeType& output_shape = {});
 
   /**
    * @brief Compute the convolution-2d.
