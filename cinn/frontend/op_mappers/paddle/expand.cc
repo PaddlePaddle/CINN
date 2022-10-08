660d6b6c2641d74b6b50e442a99df5c70ee11154
Aganlengzi <aganlengzi@gmail.com>
2022-10-08 03:06:58 +0000

    test expand

--- cinn/frontend/op_mappers/paddle/expand.cc
+++ cinn/frontend/op_mappers/paddle/expand.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2021 CINN Authors. All Rights Reserved.
+// Copyright (c) 2022 CINN Authors. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,18 +12,43 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <absl/types/optional.h>
-
-#include <string>
-
-#include "cinn/common/context.h"
 #include "cinn/frontend/op_mapper_registry.h"
 #include "cinn/frontend/op_mappers/common_utils.h"
-#include "cinn/frontend/syntax.h"
 
 namespace cinn {
 namespace frontend {
 namespace paddle_mappers {
+
+void ExpandOpMapper(const paddle::cpp::OpDesc& op_desc, const OpMapperContext& ctx) {
+  CHECK_EQ(op_desc.Input("X").size(), 1UL);
+  auto x_name = op_desc.Input("X").front();
+  CHECK_EQ(op_desc.Output("Out").size(), 1UL);
+  auto out_name = op_desc.Output("Out").front();
+
+  CHECK(op_desc.HasAttr("expand_times"));
+  auto expand_times = utils::GetAttrOrDefault<std::vector<int>>(op_desc, "expand_times");
+
+  auto x       = ctx.GetVar(x_name);
+  auto x_shape = x->shape;
+
+  VLOG(4) << "expand: x shape: " << cinn::utils::Join(x_shape, ", ");
+  VLOG(4) << "expand: attr shape: " << cinn::utils::Join(shape, ", ");
+
+  CHECK_EQ(shape.size(), x_shape.size()) << "The size of `expand_times' should == the rank[" << x_shape.size()
+                                         << "] of x's shape, but got " << shape.size();
+
+  std::vector<int> out_shape(x_shape.size());
+  for (size_t i = 0; i < x_shape.size(); ++i) {
+    out_shape[i] = x_shape[i] * expand_times[i];
+  }
+
+  VLOG(4) << "expand: out shape: " << cinn::utils::Join(out_shape, ", ");
+
+  auto out = ctx.Builder()->BroadcastTo(x, out_shape);
+
+  ctx.AddVar(out_name, out);
+  ctx.AddVarModelToProgram(out_name, out->id);
+}
 
 void ExpandV2OpMapper(const paddle::cpp::OpDesc& op_desc, const OpMapperContext& ctx) {
   CHECK_EQ(op_desc.Input("X").size(), 1UL);
@@ -32,36 +57,46 @@ void ExpandV2OpMapper(const paddle::cpp::OpDesc& op_desc, const OpMapperContext&
   auto out_name = op_desc.Output("Out").front();
 
   CHECK(op_desc.HasAttr("shape"));
-  auto expand_shape = utils::GetAttrOrDefault<std::vector<int>>(op_desc, "shape");
+  auto shape = utils::GetAttrOrDefault<std::vector<int>>(op_desc, "shape");
 
-  auto x = ctx.GetVar(x_name);
+  auto x       = ctx.GetVar(x_name);
+  auto x_shape = x->shape;
 
-  CHECK_GE(expand_shape.size(), x->shape.size())
-      << "The number of elements of 'shape' for "
-         "expand_v2 op must be greater than or equal to the rank of the input.";
+  VLOG(4) << "expand_v2: x shape: " << cinn::utils::Join(x_shape, ", ");
+  VLOG(4) << "expand_v2: attr shape: " << cinn::utils::Join(shape, ", ");
 
-  auto vec_in_dims = x->shape;
-  auto diff        = expand_shape.size() - vec_in_dims.size();
-  vec_in_dims.insert(vec_in_dims.begin(), diff, 1);
-  std::vector<int> out_shape(vec_in_dims.size());
+  CHECK_GE(shape.size(), x_shape.size()) << "The size of `shape' should >= the rank[" << x_shape.size()
+                                         << "] of x's shape, but got " << shape.size();
 
-  for (size_t i = 0; i < vec_in_dims.size(); ++i) {
-    CHECK_NE(expand_shape[i], 0) << "The expanded size cannot be zero.";
+  auto diff = shape.size() - x_shape.size();
+  x_shape.insert(x_shape.begin(), diff, 1);
+
+  std::vector<int> out_shape(x_shape.size());
+
+  for (size_t i = 0; i < x_shape.size(); ++i) {
+    CHECK_NE(shape[i], 0) << "The " << i << "th element in shape cannot be zero.";
     if (i < diff) {
-      CHECK_GT(expand_shape[i], 0) << "The expanded size for non-existing dimensions must be positive.";
-      out_shape[i] = expand_shape[i];
-    } else if (expand_shape[i] > 0) {
-      if (vec_in_dims[i] != 1) {
-        CHECK_EQ(expand_shape[i], expand_shape[i]);
-        out_shape[i] = expand_shape[i];
+      CHECK_GT(shape[i], 0) << "The " << i << "th element[" << shape[i]
+                            << "] for non-existing dimensions must be positive.";
+      out_shape[i] = shape[i];
+    } else if (shape[i] > 0) {
+      if (x_shape[i] != 1) {
+        CHECK_EQ(shape[i], x_shape[i]) << "The " << i << "th element[" << shape[i]
+                                       << "] of the non-singleton dimension does not match"
+                                          " the corresponding element["
+                                       << x_shape[i] << "] in x's shape.";
+        out_shape[i] = shape[i];
       } else {
-        out_shape[i] = expand_shape[i];
+        out_shape[i] = shape[i];
       }
     } else {
-      CHECK_EQ(expand_shape[i], -1) << "When the value in shape is negative for expand_v2 op, only -1 is supported";
-      out_shape[i] = vec_in_dims[i];
+      CHECK_EQ(shape[i], -1) << "When the element in shape is negative for expand_v2 op, only -1 is supported, but got "
+                             << shape[i];
+      out_shape[i] = x_shape[i];
     }
   }
+
+  VLOG(4) << "expand_v2: out shape: " << cinn::utils::Join(out_shape, ", ");
 
   auto out = ctx.Builder()->BroadcastTo(x, out_shape);
 
@@ -74,6 +109,7 @@ void ExpandV2OpMapper(const paddle::cpp::OpDesc& op_desc, const OpMapperContext&
 }  // namespace cinn
 
 CINN_REGISTER_HELPER(paddle_expand) {
+  CINN_REGISTER_OP_MAPPER(expand, cinn::frontend::paddle_mappers::ExpandOpMapper)
   CINN_REGISTER_OP_MAPPER(expand_v2, cinn::frontend::paddle_mappers::ExpandV2OpMapper)
   return true;
 }
