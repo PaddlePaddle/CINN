cae0c41f7c6195df3a4a63b0d1fd0963e509fd33
CtfGo <ctfeng66@163.com>
2022-10-12 03:20:24 +0000

    add IrEqualVisitor to compare a pair Expr is equal

---
+++ cinn/ir/ir_compare.h
@@ -0,0 +1,36 @@
+// Copyright (c) 2022 CINN Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+#include <vector>
+
+#include "cinn/ir/ir.h"
+#include "cinn/ir/ir_visitor.h"
+
+namespace cinn {
+namespace ir {
+
+struct IrEqualVistor : public IRVisitorBase<bool, const Expr*> {
+  bool Compare(const Expr& lhs, const Expr& rhs);
+#define __(op__) bool Visit(const op__* lhs, const Expr* other) override;
+  NODETY_FORALL(__)
+#undef __
+ private:
+  bool Compare(const std::map<std::string, attr_t>& lhs, const std::map<std::string, attr_t>& rhs);
+  template <typename T>
+  bool Compare(const std::vector<T>& lhs, const std::vector<T>& rhs);
+};
+
+}  // namespace ir
+}  // namespace cinn
