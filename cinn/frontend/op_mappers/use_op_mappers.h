bc3c9832aea24d9039554544ab084dbb9211c3ef
石晓伟 <39303645+Shixiaowei02@users.noreply.github.com>
2022-10-13 17:18:18 +0800

    adds the operation mapper for clip (#989)
    * adds the operation mapper for clip

--- cinn/frontend/op_mappers/use_op_mappers.h
+++ cinn/frontend/op_mappers/use_op_mappers.h
@@ -39,6 +39,7 @@ CINN_USE_REGISTER(paddle_constant)
 CINN_USE_REGISTER(paddle_where)
 CINN_USE_REGISTER(paddle_layer_norm)
 CINN_USE_REGISTER(paddle_squeeze)
+CINN_USE_REGISTER(paddle_clip)
 CINN_USE_REGISTER(paddle_unsqueeze)
 CINN_USE_REGISTER(paddle_expand)
 
