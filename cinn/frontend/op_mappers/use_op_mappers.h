8d0467b96e518534d81d1cb2c0a2d6b8debdab35
Wilber <jiweibo@baidu.com>
2022-10-13 20:32:00 +0800

    support paddle take_along_axis op (#993)

--- cinn/frontend/op_mappers/use_op_mappers.h
+++ cinn/frontend/op_mappers/use_op_mappers.h
@@ -42,6 +42,7 @@ CINN_USE_REGISTER(paddle_squeeze)
 CINN_USE_REGISTER(paddle_clip)
 CINN_USE_REGISTER(paddle_unsqueeze)
 CINN_USE_REGISTER(paddle_expand)
+CINN_USE_REGISTER(paddle_take_along_axis)
 
 CINN_USE_REGISTER(science_broadcast)
 CINN_USE_REGISTER(science_transform)
