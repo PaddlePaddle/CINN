0b1b4291731b04ad4e6cd9ac99014ea30eae2ad2
Aganlengzi <aganlengzi@gmail.com>
2022-10-21 13:16:37 +0000

    fix transpose2 with uts

---
+++ python/tests/op_mappers/test_transpose2_op.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2022 CINN Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import sys
+import unittest
+import numpy as np
+from op_mapper_test import OpMapperTest
+import paddle
+from cinn.frontend import *
+from cinn.common import *
+
+paddle.enable_static()
+
+enable_gpu = sys.argv.pop()
+
+
+class TestTranspose2Op(OpMapperTest):
+    def setUp(self):
+        if enable_gpu == "ON":
+            self.target = DefaultNVGPUTarget()
+            self.place = paddle.CUDAPlace(0)
+        else:
+            self.target = DefaultHostTarget()
+            self.place = paddle.CPUPlace()
+
+    def init_input_dtype(self):
+        self.dtype = 'float32'
+
+    def init_input_data(self):
+        self.init_input_dtype()
+        self.feed_data = {
+            'x': self.random([2, 3, 4], self.dtype),
+        }
+
+    def set_paddle_program(self):
+        x = paddle.static.data(name='x', shape=[2, 3, 4], dtype=self.dtype)
+        perm = [1, 0, 2]
+        out = paddle.transpose(x, perm)
+
+        return ([x.name], [out])
+
+    def test_check_results(self):
+        self.check_outputs_and_grads()
+
+
+class TestTranspose2OpInt32(TestTranspose2Op):
+    def init_input_dtype(self):
+        self.dtype = 'float32'
+
+
+class TestTranspose2OpInt64(TestTranspose2Op):
+    def init_input_dtype(self):
+        self.dtype = 'int64'
+
+
+if __name__ == "__main__":
+    unittest.main()
