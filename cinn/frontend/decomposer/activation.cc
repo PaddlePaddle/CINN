087317e589690830a63236d3237bb3ea52d7c60c
jiangcheng <thisjiang@qq.com>
2022-12-01 19:03:13 +0800

    fix decomposer fill_constant no dtype bug (#1091)
    * fix decomposer fill_constant no dtype bug
    
    * fix batch norm fill constant bug
    
    * fix batch norm bug

--- cinn/frontend/decomposer/activation.cc
+++ cinn/frontend/decomposer/activation.cc
@@ -26,7 +26,7 @@ void relu(const Instruction& instr, const DecomposerContext& context) {
   auto output   = instr->outputs[0];
   auto* builder = context.builder();
 
-  auto bcast_zero = builder->FillConstant<float>(x->shape, 0.0f, common::UniqName("zero"));
+  auto bcast_zero = builder->FillConstant(x->shape, 0.0f, common::UniqName("zero"), common::Type2Str(x->type));
   auto out        = builder->Max(x, bcast_zero);
 
   // map the the output of decomposed operator to the original.
@@ -41,7 +41,7 @@ void relu_grad(const Instruction& instr, const DecomposerContext& context) {
   auto dx       = instr->outputs[0];
   auto* builder = context.builder();
 
-  auto bcast_zero = builder->FillConstant<float>(out->shape, 0.0f, common::UniqName("zero"));
+  auto bcast_zero = builder->FillConstant(out->shape, 0.0f, common::UniqName("zero"), common::Type2Str(out->type));
   auto condition  = builder->GreaterThan(out, bcast_zero);
   auto res        = builder->Select(condition, dout, bcast_zero);
 
@@ -57,8 +57,8 @@ void gelu(const Instruction& instr, const DecomposerContext& context) {
   auto* builder = context.builder();
 
   // x * (0.5 + 0.5 * erf(sqrtf(0.5) * x))
-  auto p_5 = builder->FillConstant<float>(x->shape, 0.5f, common::UniqName("p_5"));
-  auto p_7 = builder->FillConstant<float>(x->shape, std::sqrt(0.5), common::UniqName("p_7"));
+  auto p_5 = builder->FillConstant(x->shape, 0.5f, common::UniqName("p_5"), common::Type2Str(x->type));
+  auto p_7 = builder->FillConstant(x->shape, std::sqrt(0.5), common::UniqName("p_7"), common::Type2Str(x->type));
   auto erf = builder->Erf(builder->Multiply(x, p_7));
   auto cdf = builder->Add(p_5, builder->Multiply(p_5, erf));
   auto out = builder->Multiply(x, cdf);
