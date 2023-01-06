c2f54f0a22163259ea956466b182be7a01ce155d
enkilee <luhputu0815@gmail.com>
2023-01-06 08:55:30 +0000

    refix

--- cinn/runtime/buffer.cc
+++ cinn/runtime/buffer.cc
@@ -26,7 +26,8 @@ Shape::Shape(const Shape &other) : data_(new value_type[other.ndims()]), ndims_(
 void Shape::Resize(int ndim) {
   CHECK_GT(ndim, 0);
   ndims_ = ndim;
-  data_  = new value_type[ndim];
+  delete data_;
+  data_ = new value_type[ndim];
 }
 
 Shape::value_type &Shape::operator[](int i) {
