c44e080435bf4c4aa8807683453a8f937c782939
CtfGo <ctfeng66@163.com>
2023-03-01 12:33:09 +0000

    try to fix ci

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 # Copyright (c) 2021 CINN Authors. All Rights Reserved.
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -104,7 +104,7 @@ function prepare_ci {
     python${py_version} -m venv $build_dir/ci-env
   fi
   source $build_dir/ci-env/bin/activate
-  pip install -U pip
+  # pip install -U pip
   pip install pre-commit
   pip install clang-format==9.0
   pip install wheel
