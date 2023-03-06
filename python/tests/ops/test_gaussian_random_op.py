1518c155d81e947365eb921ab2adf75f674b5090
jiangcheng <thisjiang@qq.com>
2023-03-06 20:52:16 +0800

    fix gaussian api bug

--- python/tests/ops/test_gaussian_random_op.py
+++ python/tests/ops/test_gaussian_random_op.py
@@ -37,8 +37,8 @@ class TestGaussianRandomOp(OpTest):
         self.dtype = "float32"
 
     def build_paddle_program(self, target):
-        out = paddle.gaussian(self.shape, self.mean, self.std, self.seed,
-                              self.dtype)
+        out = paddle.tensor.random.gaussian(self.shape, self.mean, self.std,
+                                            self.seed, self.dtype)
         self.paddle_outputs = [out]
 
     def build_cinn_program(self, target):
