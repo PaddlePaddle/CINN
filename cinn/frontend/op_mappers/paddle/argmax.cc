0967e923ceb98b0e47ba3f9fb7bad2cf09b45cbd
GGBond8488 <857631483@qq.com>
2023-03-09 11:11:40 +0000

    fix utils error

--- cinn/frontend/op_mappers/paddle/argmax.cc
+++ cinn/frontend/op_mappers/paddle/argmax.cc
@@ -16,6 +16,7 @@
 
 #include "cinn/frontend/op_mapper_registry.h"
 #include "cinn/frontend/op_mappers/common_utils.h"
+#include "cinn/frontend/var_type_utils.h"
 
 namespace cinn {
 namespace frontend {
