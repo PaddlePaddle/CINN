af89cf7b8cf230cbeb242686252c070e3dc9661d
zhengqiwen1997 <zhengqiwen_au@163.com>
2023-03-09 14:34:05 +0000

    the use of Split was wrong

--- cinn/frontend/op_mappers/paddle/roll.cc
+++ cinn/frontend/op_mappers/paddle/roll.cc
@@ -71,7 +71,7 @@ void RollOpMapper(const paddle::cpp::OpDesc& op_desc, const OpMapperContext& ctx
   // use Split + Concat for each shift
   for (int i = 0; i < shifts_size; ++i) {
     int length        = vec_x_dims[axis[i]];
-    auto split_output = ctx.Builder()->Split(output, {length - shifts[i]}, axis[i]);
+    auto split_output = ctx.Builder()->Split(output, {length - shifts[i], -1}, axis[i]);
     std::swap(split_output[0], split_output[1]);
     output = ctx.Builder()->Concat(split_output, axis[i]);
   }
