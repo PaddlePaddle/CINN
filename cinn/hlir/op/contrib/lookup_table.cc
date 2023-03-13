f1cb1344a579ad3963c25e2104cf182ef443ee38
GaoYuYang <zdlnlhmj@126.com>
2023-03-13 11:14:03 +0000

    change lookuptable to kInjective

--- cinn/hlir/op/contrib/lookup_table.cc
+++ cinn/hlir/op/contrib/lookup_table.cc
@@ -143,6 +143,6 @@ CINN_REGISTER_HELPER(lookup_table_ops) {
       .set_attr<cinn::hlir::framework::StrategyFunction>("CINNStrategy", cinn::hlir::op::StrategyForLookupTable)
       .set_attr("infershape", MakeOpFunction(cinn::hlir::op::InferShapeForLookupTable))
       .set_attr("inferdtype", MakeOpFunction(cinn::hlir::op::InferDtypeForLookupTable))
-      .set_attr<cinn::hlir::framework::OpPatternKind>("OpPattern", cinn::hlir::framework::OpPatternKind::kOutFusible);
+      .set_attr<cinn::hlir::framework::OpPatternKind>("OpPattern", cinn::hlir::framework::OpPatternKind::kInjective);
   return true;
 }
