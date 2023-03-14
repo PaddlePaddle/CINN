742ddc48fb860ed5e7aad11d19fb73887f14a1d7
jiangcheng <thisjiang@qq.com>
2023-03-14 14:04:40 +0800

    fix reduce may has no out_dtype bug

--- cinn/frontend/op_mappers/paddle/reduce.cc
+++ cinn/frontend/op_mappers/paddle/reduce.cc
@@ -58,13 +58,15 @@ void ReduceOpMapper(const paddle::cpp::OpDesc& op_desc, const OpMapperContext& c
 
   CHECK(out) << "Not support Reduce " << reduce_type << "! Please check.";
 
-  auto dtype_id =
-      utils::GetAttrOrDefault<int>(op_desc, "out_dtype", static_cast<int>(paddle::cpp::VarDescAPI::Type::FP32));
-  auto dtype_pd   = static_cast<paddle::cpp::VarDescAPI::Type>(dtype_id);
-  auto dtype_cinn = utils::CppVarType2CommonType(dtype_pd);
-  auto dtype      = common::Type2Str(dtype_cinn);
-  if (out.value()->type != dtype_cinn) {
-    out = ctx.Builder()->Cast(out.value(), dtype);
+  if (op_desc.HasAttr("out_dtype")) {
+    auto dtype_id =
+        utils::GetAttrOrDefault<int>(op_desc, "out_dtype", static_cast<int>(paddle::cpp::VarDescAPI::Type::FP32));
+    auto dtype_pd   = static_cast<paddle::cpp::VarDescAPI::Type>(dtype_id);
+    auto dtype_cinn = utils::CppVarType2CommonType(dtype_pd);
+    auto dtype      = common::Type2Str(dtype_cinn);
+    if (out.value()->type != dtype_cinn) {
+      out = ctx.Builder()->Cast(out.value(), dtype);
+    }
   }
 
   ctx.AddVar(out_name, out.value());
