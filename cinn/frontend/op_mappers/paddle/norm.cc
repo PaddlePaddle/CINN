a9003b0fb74d253dd7f9b8aade12a58faba92aab
jiangcheng <thisjiang@qq.com>
2023-03-16 17:26:23 +0800

    add Norm output for norm op (#1259)
    * add Norm output for norm op
    
    * fix reduce may has no out_dtype bug
    
    * fix reduce may has no out_dtype bug
    
    * fix reduce out_dtype attribute default value should be -1 bug
    
    * fix FLAGS_cinn_fusion_groups_graphviz_dir useless bug
    
    * fix by review advices
    
    * scale tensor need cast

--- cinn/frontend/op_mappers/paddle/norm.cc
+++ cinn/frontend/op_mappers/paddle/norm.cc
@@ -19,21 +19,84 @@ namespace cinn {
 namespace frontend {
 namespace paddle_mappers {
 
+struct NormHelper {
+  NormHelper(NetBuilder* net_builder, int32_t axis) {
+    builder          = net_builder;
+    reduce_dim       = {axis};
+    num_instructions = builder->size();
+  }
+
+  ~NormHelper() { VLOG(4) << "norm is decomposed to " << builder->size() - num_instructions << " instructions."; }
+
+  // square_sum = reduce_sum(x * x)
+  Variable SquareSum(Variable x) {
+    auto x_square     = builder->Multiply(x, builder->Identity(x));
+    auto x_square_sum = Reduce(x_square);
+
+    return x_square_sum;
+  }
+
+  // std_square_sum = sqrt(square_sum + epsilon)
+  Variable StdSquareSum(Variable square_sum, float epsilon) {
+    auto epsilon_1d = builder->FillConstant(
+        square_sum->shape, epsilon, common::UniqName("norm_epsilon"), common::Type2Str(square_sum->type));
+    auto std_square_sum = builder->Sqrt(builder->Add(square_sum, epsilon_1d));
+    return std_square_sum;
+  }
+
+  Variable Reduce(Variable x) { return builder->ReduceSum(x, reduce_dim, true); }
+
+  NetBuilder* builder{nullptr};
+  std::vector<int> reduce_dim;
+  int num_instructions{0};
+};
+
 void NormOpMapper(const paddle::cpp::OpDesc& op_desc, const OpMapperContext& ctx) {
   CHECK_EQ(op_desc.Input("X").size(), 1UL);
   auto x_name = op_desc.Input("X").front();
   CHECK_EQ(op_desc.Output("Out").size(), 1UL);
   auto out_name = op_desc.Output("Out").front();
-  auto x        = ctx.GetVar(x_name);
+
+  std::string norm_name;
+  if (op_desc.HasOutput("Norm") && !op_desc.Output("Norm").empty()) {
+    CHECK_EQ(op_desc.Output("Norm").size(), 1UL);
+    norm_name = op_desc.Output("Norm").front();
+  }
 
   CHECK(op_desc.HasAttr("axis"));
-  CHECK(op_desc.HasAttr("epsilon"));
-  auto axis    = op_desc.GetAttr<int32_t>("axis");
-  auto epsilon = op_desc.GetAttr<float>("epsilon");
-  auto out     = ctx.Builder()->Norm(x, axis, epsilon);
+  auto axis    = utils::GetAttrOrDefault<int>(op_desc, "axis", -1);
+  auto epsilon = utils::GetAttrOrDefault<float>(op_desc, "epsilon", 1.0e-10f);
+  auto is_test = utils::GetAttrOrDefault<bool>(op_desc, "is_test", norm_name.empty());
 
-  ctx.AddVar(out_name, out);
-  ctx.AddVarModelToProgram(out_name, out->id);
+  auto x = ctx.GetVar(x_name);
+
+  VLOG(4) << "Out=" << out_name << ", Norm=" << norm_name << " = norm(X:" << x_name << "=" << x << ", axis=" << axis
+          << ", epsilon=" << epsilon << ", is_test=" << std::ios::boolalpha << is_test;
+
+  if (axis < 0) {
+    axis += x->shape.size();
+  }
+  CHECK_GE(axis, 0);
+  CHECK_LT(axis, x->shape.size());
+
+  NormHelper helper(ctx.Builder(), axis);
+
+  auto in_type = x->type;
+  if (in_type.is_float(16)) {
+    x = ctx.Builder()->Cast(x, "float32");
+  }
+  auto square_sum     = helper.SquareSum(x);
+  auto std_square_sum = helper.StdSquareSum(square_sum, epsilon);
+  auto normalized     = ctx.Builder()->Divide(x, std_square_sum);
+  auto y              = ctx.Builder()->Cast(normalized, common::Type2Str(in_type));
+
+  ctx.AddVar(out_name, y);
+  ctx.AddVarModelToProgram(out_name, y->id);
+
+  if (!norm_name.empty()) {
+    ctx.AddVar(norm_name, std_square_sum);
+    ctx.AddVarModelToProgram(norm_name, std_square_sum->id);
+  }
 }
 
 }  // namespace paddle_mappers
