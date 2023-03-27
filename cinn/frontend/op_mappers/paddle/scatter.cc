0896a5b161ccd297c79e2796ffa2e64f4fe0bae8
zzk0 <zzk9460@gmail.com>
2023-03-27 08:24:07 +0000

    cast int64 to int32

--- cinn/frontend/op_mappers/paddle/scatter.cc
+++ cinn/frontend/op_mappers/paddle/scatter.cc
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include "cinn/common/type.h"
 #include "cinn/frontend/op_mapper_registry.h"
 #include "cinn/frontend/op_mappers/common_utils.h"
 
@@ -29,18 +30,25 @@ void ScatterOpMapper(const paddle::cpp::OpDesc& op_desc, const OpMapperContext& 
   CHECK_EQ(op_desc.Output("Out").size(), 1UL);
   auto out_name = op_desc.Output("Out").front();
 
-  auto overwrite = utils::GetAttrOrDefault<bool>(op_desc, "overwrite", true);
+  bool overwrite = utils::GetAttrOrDefault<bool>(op_desc, "overwrite", true);
   VLOG(4) << "out_name = scatter(X=" << x_name << ", Ids=" << ids_name << ", Updates=" << updates_name
           << ", overwrite=" << overwrite << ")";
 
-  auto input   = ctx.GetVar(x_name);
-  auto indices = ctx.GetVar(ids_name);
-  auto updates = ctx.GetVar(updates_name);
+  const auto& input   = ctx.GetVar(x_name);
+  auto indices        = ctx.GetVar(ids_name);
+  const auto& updates = ctx.GetVar(updates_name);
+  CHECK(input->type == updates->type) << "checks whether the type of the input and the updates are the same.";
+  CHECK(indices->type == common::Int(32) || indices->type == common::Int(64))
+      << "checks whether the data type of the indices is either int32 or int64";
+  if (indices->type == common::Int(64)) {
+    indices = ctx.Builder()->Cast(indices, common::Type2Str(common::Int(32)));
+  }
+
   Variable out;
   if (overwrite) {
     out = ctx.Builder()->ScatterAssign(input, updates, indices);
   } else {
-    auto zeros = ctx.Builder()->FillConstant(
+    const auto& zeros = ctx.Builder()->FillConstant(
         updates->shape, 0, common::UniqName("scatter_zeros"), common::Type2Str(updates->type));
     out = ctx.Builder()->ScatterAssign(input, zeros, indices);
     out = ctx.Builder()->ScatterAdd(out, updates, indices);
