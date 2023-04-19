2875d486513a3566deb45570d65ecd8dd3c8476d
sunli <466530738@qq.com>
2023-04-19 09:17:50 +0000

    compute buffer size on gpu

--- cinn/optim/transform_gpu_forloop.h
+++ cinn/optim/transform_gpu_forloop.h
@@ -25,44 +25,12 @@
 namespace cinn {
 namespace optim {
 
-using forloop_infos_t = std::map<std::string, std::map<std::string, poly::StageForloopInfo>>;
-
-/**
- * Collect the grid and block dims from a group of stages.
- * The dims is the maximum extent of each GPU related forloops.
- */
-ir::CudaAxisInfo GatherAxisInfoFromStages(const std::vector<poly::Stage*>& stage_group);
-
-/**
- * Mark the fortype and device of forloops if is GPU related, replace the loop iterators to GPU related axis(threadIdx.x
- * and so on).
- *
- * For example, input the code
- * \code
- * for (i, 0, 10)
- *   for (j, 0, 20)
- *     A(i,j)
- * \endcode
- *
- * with the `i` set as CUDA block axis, `j` set as CUDA thread axis, the original forloop will be modified to
- *
- * \code
- * for (blockIdx.x, 0, 10)
- *   for (threadIdx.x, 0, 20)
- *     A(blockIdx.x, threadIdx.x)
- * \endcode
- *
- * @param expr The expression to modify.
- * @param global_tensor_map The map mapping a tensor's name to itself.
- * @param forloop_infos A map of forloop to their infomation.
- */
-void TransformGpuForloops(const forloop_infos_t& forloop_infos,
-                          const std::vector<std::string>& traverse_order,
-                          std::map<std::string, ir::Tensor>* global_tensor_map,
-                          std::unordered_map<std::string, std::vector<Expr>>& resized_buffer_cache,
-                          Expr* expr);
-
 void OptimizeExprGPU(Expr* expr);
+/*
+  // replace 'for' loop to gpu 'block/thread'
+  // update buffer index to save memory size.
+  // re-compute buffer size.
+*/
 
 /**
  * Remove the forloops of block and thread axis, add the kernel launch thread dimension information to the outermost
