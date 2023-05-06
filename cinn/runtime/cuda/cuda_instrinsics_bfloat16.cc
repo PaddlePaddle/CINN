4e1f42f4fc438ebf3831322b34b4fcb7ae3f0636
lanxianghit <lanxiang@outlook.com>
2023-05-06 07:22:46 +0000

    fix clang format

--- cinn/runtime/cuda/cuda_instrinsics_bfloat16.cc
+++ cinn/runtime/cuda/cuda_instrinsics_bfloat16.cc
@@ -15,8 +15,8 @@
 #include "cinn/backends/cuda_util.h"
 #include "cinn/backends/extern_func_jit_register.h"
 #include "cinn/backends/function_prototype.h"
-#include "cinn/common/cas.h"
 #include "cinn/common/bfloat16.h"
+#include "cinn/common/cas.h"
 #include "cinn/runtime/cuda/cuda_util.h"
 #include "cinn/runtime/custom_function.h"
 
