385f8c588bda3c18750c5848e95fef22c392b92d
Fisher <fisheryung@outlook.com>
2023-05-22 07:21:54 +0000

    Skip pool2d op mapper tests if is not build with cudnn

--- python/tests/op_mappers/test_pool2d_op.py
+++ python/tests/op_mappers/test_pool2d_op.py
@@ -13,11 +13,13 @@
 # limitations under the License.
 
 import unittest
-import numpy as np
-from op_mapper_test import OpMapperTest, logger
 import paddle
+from op_mapper_test import OpMapperTest
+from cinn.common import *
 
 
+@unittest.skipIf(not is_compiled_with_cudnn(),
+                 "x86 test will be skipped due to timeout.")
 class TestPool2dOp(OpMapperTest):
     def init_input_data(self):
         self.feed_data = {"x": self.random([2, 3, 7, 7], "float64")}
