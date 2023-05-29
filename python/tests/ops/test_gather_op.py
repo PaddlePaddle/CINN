cd2571e46724e6d9b57a2137ab3b5f04aa985792
zrr1999 <2742392377@qq.com>
2023-05-29 14:47:50 +0000

    use TestCaseHelper

--- python/tests/ops/test_gather_op.py
+++ python/tests/ops/test_gather_op.py
@@ -17,6 +17,7 @@
 import unittest
 import numpy as np
 from op_test import OpTest, OpTestTool
+from op_test_helper import TestCaseHelper
 import paddle
 import cinn
 from cinn.frontend import *
@@ -33,50 +34,49 @@ logger = logging.getLogger(name="gather")
                     "x86 test will be skipped due to timeout.")
 class TestGatherOp(OpTest):
     def setUp(self):
-        self.data = []
-        self.init_case()
-
-    def init_case(self):
-        self.inputs = [{"x": [3, 4, 3], "index": [4], "axis": 1}]
-        self.dtypes = ["float32"]
+        print(f"\nRunning {self.__class__.__name__}: {self.case}")
+        self.data = None
 
     def build_paddle_program(self, target):
-        for inputs, dtype in product(self.inputs, self.dtypes):
-            axis = inputs["axis"]
-            x_shape = inputs["x"]
-            index_shape = inputs["index"]
-            # Paddle does not support negative axis values.
-            axis = axis if axis >= 0 else len(x_shape) + axis
-            x = np.random.randn(*x_shape).astype(dtype)
-            index = np.random.randint(0, x_shape[axis],
-                                      index_shape).astype("int32")
-            self.data.append([x, index])
-            x = paddle.to_tensor(x, stop_gradient=False)
-            index = paddle.to_tensor(index, stop_gradient=False)
-            out = paddle.gather(x, index, axis)
-            logger.debug(" -- The output of Paddle:\n{}".format(out))
-            self.paddle_outputs.append(out)
+        inputs = self.case
+        dtype = self.case["x_dtype"]
+        axis = inputs["axis"]
+        x_shape = inputs["x"]
+        index_shape = inputs["index"]
+        # Paddle does not support negative axis values.
+        axis = axis if axis >= 0 else len(x_shape) + axis
+        x = np.random.randn(*x_shape).astype(dtype)
+        index = np.random.randint(0, x_shape[axis],
+                                  index_shape).astype("int32")
+        self.data = [x, index]
+        x = paddle.to_tensor(x, stop_gradient=False)
+        index = paddle.to_tensor(index, stop_gradient=False)
+        out = paddle.gather(x, index, axis)
+        logger.debug(" -- The output of Paddle:\n{}".format(out))
+        self.paddle_outputs.append(out)
 
     def build_cinn_program(self, target):
-        for i, (inputs, dtype) in enumerate(product(self.inputs, self.dtypes)):
-            axis = inputs["axis"]
-            builder = NetBuilder("gather")
-            x = builder.create_input(
-                self.nptype2cinntype(dtype), inputs["x"], "x")
-            index = builder.create_input(Int(32), inputs["index"], "index")
-            out = builder.gather(x, index, axis=axis)
-            prog = builder.build()
-            res = self.get_cinn_output(prog, target, [x, index], self.data[i],
-                                       [out])
-            logger.debug(" -- The output of CINN:\n{}".format(res))
-            self.cinn_outputs.extend(res)
+        inputs = self.case
+        dtype = self.case["x_dtype"]
+        axis = inputs["axis"]
+        builder = NetBuilder("gather")
+        x = builder.create_input(self.nptype2cinntype(dtype), inputs["x"], "x")
+        index = builder.create_input(Int(32), inputs["index"], "index")
+        out = builder.gather(x, index, axis=axis)
+        prog = builder.build()
+        res = self.get_cinn_output(prog, target, [x, index], self.data, [out])
+        logger.debug(" -- The output of CINN:\n{}".format(res))
+        self.cinn_outputs.extend(res)
 
     def test_check_results(self):
         self.check_outputs_and_grads(all_equal=True)
 
 
-class TestGatherOpAll(TestGatherOp):
-    def init_case(self):
+class TestGatherOpAll(TestCaseHelper):
+    def init_attrs(self):
+        self.class_name = "TestGatherOpAll"
+        self.cls = TestGatherOp
+        # note: The possible values of axis are related to x, so axis is added in self.inputs
         self.inputs = [
             {
                 "x": [128],
@@ -134,15 +134,24 @@ class TestGatherOpAll(TestGatherOp):
                 "axis": 2
             },
         ]
-        self.dtypes = [
-            "float32",
-            "float64",
-            "int16",
-            "int32",
-            "int64",
-            # "uint8"  # note: some types is not supported in paddle now.
-        ]
+        self.dtypes = [{
+            "x_dtype": "int16",
+            "y_dtype": "int64"
+        }, {
+            "x_dtype": "int32",
+            "y_dtype": "int64"
+        }, {
+            "x_dtype": "int64",
+            "y_dtype": "int64"
+        }, {
+            "x_dtype": "float32",
+            "y_dtype": "int64"
+        }, {
+            "x_dtype": "float64",
+            "y_dtype": "int64"
+        }]
+        self.attrs = []
 
 
 if __name__ == "__main__":
-    unittest.main()
+    TestGatherOpAll().run()
