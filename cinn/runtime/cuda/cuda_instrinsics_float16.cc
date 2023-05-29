f1ced9ce719df081df1c142a5bc3bc445983b4ad
zzk0 <zzk9460@gmail.com>
2023-05-29 02:54:07 +0000

    fix paddle test_arg_min_max_op: axis type & fp16

--- cinn/runtime/cuda/cuda_instrinsics_float16.cc
+++ cinn/runtime/cuda/cuda_instrinsics_float16.cc
@@ -76,7 +76,35 @@ CINN_REGISTER_HELPER(cuda_intrinsics_float16) {
 
 #undef REGISTER_EXTERN_FUNC_1_IN_1_FP16_OUT_BOOL
 
-#define _REGISTER_CINN_NVGPU_INDEX_ADD(TYPE_SUFFIX, TYPE)                        \
+#define REGISTER_CINN_NVGPU_GT_NUM(TYPE_SUFFIX, TYPE)                            \
+  REGISTER_FACKED_EXTERN_FUNC_HELPER(cinn_nvgpu_gt_num_##TYPE_SUFFIX, target)    \
+      .SetRetType<int>()                                                         \
+      .AddInputType<cinn_buffer_t *>()                                           \
+      .AddInputType<int>()                                                       \
+      .AddInputType<TYPE>()                                                      \
+      .AddInputType<int>()                                                       \
+      .AddInputType<int>()                                                       \
+      .End();
+
+  REGISTER_CINN_NVGPU_GT_NUM(fp16, float16);
+
+#undef REGISTER_CINN_NVGPU_GT_NUM
+
+#define REGISTER_CINN_NVGPU_LT_NUM(TYPE_SUFFIX, TYPE)                            \
+  REGISTER_FACKED_EXTERN_FUNC_HELPER(cinn_nvgpu_lt_num_##TYPE_SUFFIX, target)    \
+      .SetRetType<int>()                                                         \
+      .AddInputType<cinn_buffer_t *>()                                           \
+      .AddInputType<int>()                                                       \
+      .AddInputType<TYPE>()                                                      \
+      .AddInputType<int>()                                                       \
+      .AddInputType<int>()                                                       \
+      .End();
+
+  REGISTER_CINN_NVGPU_LT_NUM(fp16, float16);
+
+#undef REGISTER_CINN_NVGPU_LT_NUM
+
+#define REGISTER_CINN_NVGPU_INDEX_ADD(TYPE_SUFFIX, TYPE)                         \
   REGISTER_FACKED_EXTERN_FUNC_HELPER(cinn_nvgpu_index_add_##TYPE_SUFFIX, target) \
       .SetRetType<TYPE>()                                                        \
       .AddInputType<TYPE>()                                                      \
@@ -88,9 +116,9 @@ CINN_REGISTER_HELPER(cuda_intrinsics_float16) {
       .AddInputType<int>()                                                       \
       .End();
 
-  _REGISTER_CINN_NVGPU_INDEX_ADD(fp16, float16);
+  REGISTER_CINN_NVGPU_INDEX_ADD(fp16, float16);
 
-#undef _REGISTER_CINN_NVGPU_INDEX_ADD
+#undef REGISTER_CINN_NVGPU_INDEX_ADD
 
   return true;
 }
