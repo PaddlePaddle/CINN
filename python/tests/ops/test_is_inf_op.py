720fdd782124c9cf3348afeacd9e0ff40e433596
MayYouBeProsperous <ljmhz@outlook.com>
2023-06-01 12:35:34 +0800

    fix bugs

--- python/tests/ops/test_is_inf_op.py
+++ python/tests/ops/test_is_inf_op.py
@@ -34,7 +34,8 @@ class TestIsInfOp(OpTest):
         self.x_np = self.random(
             shape=self.case["x_shape"], dtype=self.case["x_dtype"])
 
-        self.x_np[0] = np.inf
+        nan_data = np.zeros(self.x_np[0].shape, dtype="float") + np.nan
+        self.x_np[0] = nan_data.astype(self.case["x_dtype"])
 
     def build_paddle_program(self, target):
         x = paddle.to_tensor(self.x_np, stop_gradient=True)
